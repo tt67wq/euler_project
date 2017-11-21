4115646f9d56c103870ecddf125db2ab32970fe2
tt67wq <807643203@qq.com>
2017-11-21 09:41:43 +0800

    117题

---
+++ elixir/euler/euler_problem117.exs
@@ -0,0 +1,70 @@
+defmodule Tiles do
+  @moduledoc """
+  https://projecteuler.net/problem=117
+  """
+  use GenServer
+
+  ### GenServer API
+  def init(state), do: {:ok, state}
+
+  def handle_call({:get, tag, key}, _from, state) do
+    case Map.fetch(state, tag) do
+      {:ok, mc} ->
+	case Map.fetch(mc, key) do
+	  {:ok, value} -> {:reply, value, state}
+	  :error -> {:reply, nil, state}
+	end
+      :error -> {:reply, nil, state}
+    end
+  end
+
+  def handle_cast({:set, tag, key, value}, state) do
+    case Map.fetch(state, tag) do
+      {:ok, mc} ->
+	{:noreply, Map.update!(state, tag, fn _ -> Map.put(mc, key, value) end)}
+      :error ->
+	{:noreply, Map.put(state, tag, %{key => value})}
+    end
+  end
+
+  def handle_cast({:drop, tag, keys}, state) do
+    case Map.fetch(state, tag) do
+      {:ok, mc} ->
+	{:noreply, Map.update!(state, tag, fn _ -> Map.drop(mc, keys) end)}
+      :error ->
+	{:noreply, state}
+    end
+  end
+
+  ### Client
+  def start_link(state \\ %{}) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def get(tag, key), do: GenServer.call(__MODULE__, {:get, tag, key})
+  def set(tag, key, value), do: GenServer.cast(__MODULE__, {:set, tag, key, value})
+  def drop(tag, keys), do: GenServer.cast(__MODULE__, {:drop, tag, keys})
+  def set_and_get(tag, key, value) do
+    set(tag, key, value)
+    value
+  end
+
+  def t(n) when n < 2, do: 1
+  def t(2), do: 2
+  def t(3), do: 4
+  def t(n), do: cache_t(n-1) + cache_t(n-2) + cache_t(n-3) + cache_t(n-4)
+
+  def cache_t(n) do
+    v = get(:t, n)
+    case v do
+      nil -> set_and_get(:t, n, t(n))
+      _ -> v
+    end
+  end
+
+  def solution() do
+    start_link()
+    t(50)
+  end
+
+end
