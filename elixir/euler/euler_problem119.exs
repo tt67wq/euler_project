d9210417b68c4ed9895ee4b19d288756c1db2d2b
tt67wq <807643203@qq.com>
2017-11-23 10:02:16 +0800

    顺利解决119

--- elixir/euler/euler_problem119.exs
+++ elixir/euler/euler_problem119.exs
@@ -3,6 +3,7 @@ defmodule DigitPowerSum do
   https://projecteuler.net/problem=119
   """
   require Logger
+  @limit 100000000000000000000
 
   def int2list(num), do: i2l(num, [])
   defp i2l(0, acc), do: acc
@@ -68,14 +69,24 @@ defmodule DigitPowerSum do
   defp s?(_ds, acc, num) when acc == num, do: true
   defp s?(ds, acc, num), do: s?(ds, acc * ds, num)
 
-  def solution(), do: sl(614656, 10)
-  defp sl(num, 30), do: num
-  defp sl(num, index) do
+
+  def generate_cands(), do: gc(2, 4, [])
+  defp gc(index, _, acc) when index > 100, do: acc |> Enum.sort() |> Enum.dedup
+  defp gc(index, bcc, acc) when bcc > @limit, do: gc(index+1, (index+1) * (index+1), acc)
+  defp gc(index, bcc, acc), do: gc(index, bcc * index, [bcc|acc])
+
+  def solution() do
+    nums = generate_cands()
+    sl(nums, 1)
+  end
+  defp sl([], index), do: index
+  defp sl([h|t], index) do
     cond do
-      satisfy?(num) ->
-	Logger.info("num: #{num}, index: #{index}")
-	sl(num+1, index+1)
-      :else -> sl(num+1, index)
+      h < 10 -> sl(t, index)
+      satisfy?(h) ->
+	Logger.info("#{index}, #{h}")
+	sl(t, index+1)
+      :else -> sl(t, index)
     end
   end
 end
