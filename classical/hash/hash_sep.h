41e5f283696bcece7705b01bc97bdfca91ef4071
tt67wq <807643203@qq.com>
2017-12-15 16:03:06 +0800

    添加hash表的分类链接实现

---
+++ classical/hash/hash_sep.h
@@ -0,0 +1,26 @@
+/* 分离链接法hash */
+struct ListNode;
+typedef int ElementType;
+typedef struct ListNode *Position;
+struct HashTbl;
+typedef struct HashTbl *HashTable;
+typedef Position List;
+
+#define MinTableSize 10
+
+HashTable Initialize(int TableSize);
+void DestroyTable(HashTable H);
+Position Find(ElementType X, HashTable H);
+void Insert(ElementType X, HashTable H);
+ElementType Retrieve(Position P);
+int Hash(ElementType X, int TableSize);
+
+struct ListNode {
+  ElementType Element;
+  Postion Next;
+};
+
+struct HashTbl {
+  int TableSize;
+  List *TheLists;
+}
