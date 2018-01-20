9477af8d22bbf64295f17a00822a8811b25d75ed
tt67wq <807643203@qq.com>
2018-01-20 13:51:47 +0800

    新建一个karastuba的代码

---
+++ classical/karastuba.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  karastuba.c
+ *
+ *    Description:  karastuba算法简单描述
+ *
+ *        Version:  1.0
+ *        Created:  2018-01-20
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wanqiang
+ *
+ * =====================================================================================
+ */
+
+/* http://blog.csdn.net/bjarnecpp/article/details/77435847 */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
+#define TRUE 1
+#define FALSE 0
