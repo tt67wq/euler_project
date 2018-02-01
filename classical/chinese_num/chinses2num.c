4e9478b2fdec8670cc3986be584d059af8dcb0bc
tt67wq <807643203@qq.com>
2018-02-01 11:24:54 +0800

    开始编写汉字到数字的转换算法

---
+++ classical/chinese_num/chinses2num.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chinses2num.c
+ *
+ *    Description:  中文转阿拉伯数字
+ *
+ *        Version:  1.0
+ *        Created:  2018-02-01
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wanqiang
+ *
+ * =====================================================================================
+ */
+/* https://segmentfault.com/a/1190000004881457 */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
+#define TRUE 1
+#define FALSE 0
+
+typedef struct _chn {
+	*char ch;
+	int num;
+} ChineseNum;
+
+char *const chnNumChar[]={"零"}
+
+int main() {
+
+	return 0;
+}
+
