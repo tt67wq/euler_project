ea4ee3a06d87340bef81012583790795d320ebd8
tt67wq <807643203@qq.com>
2018-02-01 11:34:13 +0800

    初始变量声明

--- classical/chinese_num/chinses2num.c
+++ classical/chinese_num/chinses2num.c
@@ -16,23 +16,33 @@
  */
 /* https://segmentfault.com/a/1190000004881457 */
 
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
 #define TRUE 1
 #define FALSE 0
 
 typedef struct _chn {
-	*char ch;
+	char *ch;
 	int num;
 } ChineseNum;
 
-char *const chnNumChar[]={"零"}
+typedef struct _chs {
+	char *ch;
+	int value;
+	int secUnit;
+} ChineseName;
 
-int main() {
+ChineseNum const chnNumMap[10] = {{"零", 0}, {"一", 0}, {"二", 0}, {"三", 0},
+                                  {"四", 0}, {"五", 0}, {"六", 0}, {"七", 0},
+                                  {"八", 0}, {"九", 0}};
 
-	return 0;
-}
+ChineseName const chnNameMap[5] = {{"十", 10, FALSE},
+                                  {"百", 100, FALSE},
+                                  {"千", 1000, FALSE},
+                                  {"万", 10000, TRUE},
+                                  {"亿", 100000000, FALSE}};
 
+int main() { return 0; }
