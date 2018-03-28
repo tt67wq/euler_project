3cb5778f9e7a15377a72db531d0607aa70f28cec
tt67wq <807643203@qq.com>
2018-03-28 13:22:21 +0800

    134题解出，代码仍需要理解

--- classical/pow_mod.ex
+++ classical/pow_mod.ex
@@ -1,4 +1,4 @@
-defmodule Pow do
+defmodule PowMod do
   require Integer
 
   def pow(_, 0), do: 1
@@ -12,6 +12,7 @@ defmodule Pow do
   # 同余定理
   def pow_mod(m, 1, k), do: Integer.mod(m, k)
   def pow_mod(m, 2, k), do: Integer.mod(m * m, k)
+
   def pow_mod(m, n, k) do
     t = Integer.mod(m, k)
 
@@ -27,6 +28,16 @@ defmodule Pow do
           :else ->
             ((pow_mod(m, 2, k) |> pow_mod(div(n - 1, 2), k)) * t) |> Integer.mod(k)
         end
+    end
+  end
+
+  def multi_mod(a, b, k) do
+    m1 = Integer.mod(a, k)
+    m2 = Integer.mod(b, k)
+
+    cond do
+      m1 == a and m2 == b -> Integer.mod(m1 * m2, k)
+      :else -> multi_mod(m1, m2, k)
     end
   end
 end
