c32cee445117b55cf31a2bccb6dcb401e6a7fef5
tt67wq <807643203@qq.com>
2018-03-30 12:02:00 +0800

    134题理解了，是用扩展欧几里得算法来的

--- classical/gcd.exs
+++ classical/gcd.exs
@@ -10,6 +10,7 @@ defmodule GCD do
 
   @doc """
   扩展欧几里得算法
+  https://zh.wikipedia.org/wiki/%E6%89%A9%E5%B1%95%E6%AC%A7%E5%87%A0%E9%87%8C%E5%BE%97%E7%AE%97%E6%B3%95
   """
   def extend_gcd(a, b), do: eg(a, b, {0, 1}, {1, 0})
   defp eg(a, 0, {_, lx}, {_, ly}), do: {lx, ly, a}
