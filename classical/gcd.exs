1d7fe378aa45a28a2cbc58dfd987a03b166d12aa
tt67wq <807643203@qq.com>
2018-03-30 11:53:39 +0800

    扩展欧几里得算法

--- classical/gcd.exs
+++ classical/gcd.exs
@@ -2,6 +2,20 @@ defmodule GCD do
   @moduledoc """
   求最大公约数
   """
+  @doc """
+  普通欧几里得算法
+  """
   def gcd(a, 0), do: a
   def gcd(a, b), do: gcd(b, rem(a, b))
+
+  @doc """
+  扩展欧几里得算法
+  """
+  def extend_gcd(a, b), do: eg(a, b, {0, 1}, {1, 0})
+  defp eg(a, 0, {_, lx}, {_, ly}), do: {lx, ly, a}
+
+  defp eg(a, b, {x, lx}, {y, ly}) do
+    quotient = div(a, b)
+    eg(b, rem(a, b), {lx - quotient * x, x}, {ly - quotient * y, y})
+  end
 end
