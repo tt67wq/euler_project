b54af151470b43e5b27ccf0ad1d623b5cd70fecd
tt67wq <807643203@qq.com>
2018-05-15 10:05:35 +0800

    Apparently works-for-me is a crappy excuse.

--- euler/euler_problem141.ex
+++ euler/euler_problem141.ex
@@ -6,28 +6,27 @@ defmodule Euler141 do
   require Logger
   @limit 1_000_000_000_000
 
-  def sqrt?(n), do: s?(n, 1)
-  defp s?(0, _), do: true
-  defp s?(n, _) when n < 0, do: false
-  defp s?(n, index), do: s?(n - index, index + 2)
+  # def sqrt?(n), do: s?(n, 1)
+  # defp s?(0, _), do: true
+  # defp s?(n, _) when n < 0, do: false
+  # defp s?(n, index), do: s?(n - index, index + 2)
 
-  def generate_sequnce({q, d, r, re}), do: gs({q, d, r, re}, 1, [])
+  def sqrt?(n) do
+    r = :math.sqrt(n) |> round()
+    r * r == n
+  end
+  
+  def extend({q, d, r, z}), do: ext({q, d, r, z}, 1, [])
 
-  defp gs({q, d, r, re}, index, acc) do
-    nre = index * index * q * d + index * r
+  defp ext({q, d, r, _} = h, index, acc) do
+    nz = q * d * index * index + r * index
 
     cond do
-      nre > @limit ->
-        acc
-
-      :else ->
-        cond do
-          sqrt?(nre) ->
-            gs({q, d, r, re}, index + 1, [{index * q, index * d, index * r, nre} | acc])
-
-          :else ->
-            gs({q, d, r, re}, index + 1, acc)
-        end
+      nz > @limit -> acc
+      sqrt?(nz) ->
+	Logger.info("#{inspect {q * index, d * index, r * index, nz}}")
+	ext(h, index + 1, [{q * index, d * index, r * index, nz} | acc])
+      :else -> ext(h, index + 1, acc)
     end
   end
 
@@ -56,10 +55,10 @@ defmodule Euler141 do
 
   def solution() do
     gp(2, 1, [])
-    |> Enum.reduce([], fn x, acc -> generate_sequnce(x) ++ acc end)
-    |> Enum.sort_by(fn {_, _, _, x} -> x end)
+    |> Enum.reduce([], fn x, acc -> acc ++ extend(x) end)
+    |> Enum.map(fn {_, _, _, x} -> x end)
+    |> Enum.sort()
     |> Enum.dedup()
-
-    # |> Enum.sum()
+    |> Enum.sum()
   end
 end
