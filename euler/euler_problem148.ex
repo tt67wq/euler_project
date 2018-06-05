6cc8dbfa67675cd77a532a2dd24940243fc8eba4
tt67wq <807643203@qq.com>
2018-06-05 11:21:09 +0800

    oops - thought I got that one.

--- euler/euler_problem148.ex
+++ euler/euler_problem148.ex
@@ -2,27 +2,32 @@ defmodule Euler148 do
   @moduledoc """
   https://projecteuler.net/problem=148
   """
-  @limit 1_000_000
+  @limit 1_000_000_000
   require Logger
 
-  # def cmob(_, 0, _), do: 1
-  # def cmob(n, k, _p) when n < k, do: 0
-  # def cmob(n, k, p), do: rem(cmob(n - 1, k - 1, p) + cmob(n - 1, k, p), p)
-
-  def row_mod_count(n, p), do: rmc(n, p, 0, [1], 0)
-  defp rmc(n, _p, index, _rows, acc) when index > n, do: acc
-  defp rmc(n, p, 0, _, _), do: rmc(n, p, 1, [1, 1], 1)
-
-  defp rmc(n, p, index, rows, acc) do    
-    nrows = grows(rows) |> Enum.map(fn x -> rem(x, p) end)
-    b = rows |> Enum.count(fn x -> rem(x, p) > 0 end)
-    Logger.info("rows: #{index}, count: #{b}")
-    rmc(n, p, index + 1, nrows, acc + b)
+  def non_multi_base7(n) do
+    # Logger.info("#{n}, #{b7(n, 1)}")
+    b7(n, 1)
   end
 
-  def grows(rows), do: gr(rows, [1])
-  defp gr([h], acc), do: [h | acc]
-  defp gr([h1, h2 | t], acc), do: gr([h2 | t], [h1 + h2 | acc])
+  defp b7(0, acc), do: acc
+  defp b7(n, acc), do: b7(div(n, 7), (1 + rem(n, 7)) * acc)
 
-  def solution(), do: row_mod_count(@limit - 1, 7)
+  def solution() do
+    r = rem(@limit - 1, 7)
+    st = @limit - 1 - r
+
+    s1 =
+      0..(div(@limit, 7) - 1)
+      |> Enum.map(fn x -> 28 * non_multi_base7(x) end)
+      |> Enum.sum()
+
+    Logger.info("胜利在望！")
+    s2 =
+      st..(@limit - 1)
+      |> Enum.map(fn x -> non_multi_base7(x) end)
+      |> Enum.sum()
+
+    s1 + s2
+  end
 end
