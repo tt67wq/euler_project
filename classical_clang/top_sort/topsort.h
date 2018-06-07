f3581abedfb1386130440412f3ff5504775c20e4
tt67wq <807643203@qq.com>
2018-06-07 11:02:52 +0800

    a few bits tried to escape, but we caught them

--- classical_clang/top_sort/topsort.h
+++ classical_clang/top_sort/topsort.h
@@ -2,19 +2,19 @@
 #define _TopSort_
 
 typedef struct _ENode {
-  int ivex;
-  struct _ENode *next_edge;
+        int ivex;
+        struct _ENode *next_edge;
 } ENode, *PENode;
 
 typedef struct _VNode {
-  char data;
-  PENode first_edge;
+        char data;
+        PENode first_edge;
 } VNode, *PrToVNode;
 
 typedef struct _LGraph {
-  int vexnum;
-  int edgnum;
-  PrToVNode *vexs;
+        int vexnum;
+        int edgnum;
+        PrToVNode *vexs;
 } LGraph, *PrToLGraph;
 
 int toplogical_sort(PrToLGraph G);
