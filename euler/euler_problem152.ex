316a5a33d2f48ffaf4c1896ef8903ed13be8c3f6
tt67wq <807643203@qq.com>
2018-06-25 18:35:16 +0800

    Yep, XNAMEX was right on this one.

--- euler/euler_problem152.ex
+++ euler/euler_problem152.ex
@@ -4,75 +4,48 @@ defmodule Euler152 do
   """
   require Logger
 
-  @base 2..45
-  @limit 1000
-
-  def gene_print(gene), do: gp(gene, Enum.to_list(@base), [])
-
-  defp gp([], _, acc), do: Enum.reverse(acc)
-
-  defp gp([h | t], [h1 | t1], acc) do
-    case h do
-      1 -> gp(t, t1, [h1 | acc])
-      0 -> gp(t, t1, acc)
-    end
+  def start() do
+    :ets.new(:eu152, [:named_table])
   end
 
-  def gene_express(gene) do
-    Enum.zip(@base, gene)
-    |> Enum.filter(fn {_, x} -> x > 0 end)
-    |> Enum.reduce(0, fn {x, _}, acc -> acc + 1 / (x * x) end)
-  end
+  def find(_n, _m, []), do: []
 
-  def breed(gene) do
-    1..10
-    |> Enum.map(fn _ -> mutation(gene) end)
-  end
+  def find(n, m, [h | t]) do
+    case :ets.lookup(:eu152, {n, m}) do
+      [{{^n, ^m}, value}] ->
+        value
 
-  @doc """
-  变异
-  """
-  def mutation(gene) do
-    case rem(Enum.random(1..5), 5) do
-      0 ->
-        pos = Enum.random(@base)
+      [] ->
+        # 没缓存
+        cond do
+          n * h * h - m < 0 ->
+            []
 
-        case Enum.at(gene, pos - 2) do
-          1 -> List.replace_at(gene, pos, 0)
-          0 -> List.replace_at(gene, pos, 1)
+          :else ->
+            case n * h * h - m do
+              0 ->
+                :ets.insert(:eu151, {{n, m}, [[h]]})
+                [[h]]
+
+              _ ->
+                v = find(n * h * h - m, m * h * h, t)
+
+                case v do
+                  [] ->
+                    # 没结果
+                    find(n, m, t)
+
+                  _ ->
+                    # 有结果
+                    Enum.map(v, fn x -> [h | x] end)
+                end
+            end
         end
-
-      _ ->
-        gene
     end
   end
 
-  def nature_choose(genes) do
-    genes
-    |> Enum.map(fn x -> {x, gene_express(x)} end)
-    |> Enum.sort_by(fn {_, x} -> abs(x - 0.5) end)
-    |> Enum.take(@limit)
-    |> Enum.map(fn {x, _} -> x end)
-  end
-
-  def iter(genes) do
-    genes
-    |> Enum.reduce([], fn x, acc -> breed(x) ++ acc end)
-    |> nature_choose()
-  end
-
-  def solution() do
-    init = List.duplicate(1, 44) |> List.duplicate(1000)
-
-    sl(init, 0)
-    |> Enum.uniq()
-    |> Enum.map(fn x -> gene_print(x) end)
-  end
-
-  defp sl(genes, index) when index > 100, do: genes
-
-  defp sl(genes, index) do
-    Logger.info(index)
-    sl(iter(genes), index + 1)
+  def demo() do
+    start()
+    find(1, 2, 2..45 |> Enum.to_list())
   end
 end
