12c9bf3f509a5f371853c109e3ff733f86092944
tt67wq <807643203@qq.com>
2018-06-27 13:34:23 +0800

    Derp. Fix missing constant post rename

--- euler/euler_problem152.ex
+++ euler/euler_problem152.ex
@@ -8,44 +8,15 @@ defmodule Euler152 do
     :ets.new(:eu152, [:named_table])
   end
 
-  def find(n, _, _) when n < 0, do: []
-  def find(0, _, _), do: [[]]
-  def find(_n, _m, []), do: []
+  def inverse_sqr_sum(_, {0, _}, acc), do: acc
+  def inverse_sqr_sum([], _, _acc), do: []
 
-  def find(n, m, [h | t]) do
-    case :ets.lookup(:eu152, {n, m}) do
-      [{{^n, ^m}, vs}] ->
-        vs
-
-      [] ->
-        # 缓存没命中
-        # 尝试选取h，看看是否能拿到结果
-        vs = find(n * h * h - m, m * h * h, t)
-
-        res =
-          case vs do
-            [] ->
-              # 选取h的情况下无法获得结果, 放弃h, 看后续的候选元素
-              find(n, m, t)
-
-            _ ->
-              # 选择h的情况下能获得正确结果，但是依然需要看后续的选择
-              Logger.info("#{n}, #{m}, #{inspect(vs)}")
-              Enum.map(vs, fn x -> [h | x] end)
-          end
-
-        if res == [] do
-          []
-        else
-          :ets.insert(:eu152, {{n, m}, res})
-          res
-        end
-    end
+  def inverse_sqr_sum([h | t], {n, m}, acc) do
+    inverse_sqr_sum(t, {n * h * h - m, m * h * h}, [h | acc])
   end
 
   def demo() do
-    start()
-    # find(129, 400, 2..10 |> Enum.to_list())
-    find(1, 2, 2..45 |> Enum.to_list())
+    inverse_sqr_sum([2, 3, 4, 5, 7, 12, 15, 20, 28, 35], {1, 2}, [])
+    # find(1, 2, 2..35 |> Enum.to_list())
   end
 end
