b8ea582cf1df941fc12492cee2d186a2e4bf8e6a
tt67wq <807643203@qq.com>
2018-07-16 15:16:54 +0800

    s/    /  /g

--- euler_clang/euler_problem126.c
+++ euler_clang/euler_problem126.c
@@ -5,22 +5,20 @@
 
 #define MAX 20000
 
-int f(int t, int x, int y, int z) {
-  return 4 * (t - 1) * (x + y + z + t - 2) + 2 * (x * y + y * z + x * z);
-}
+int f(int t, int x, int y, int z) { return 4 * (t - 1) * (x + y + z + t - 2) + 2 * (x * y + y * z + x * z); }
 
 int main(void) {
-  int cache[100000];
+        int cache[100000];
 
-  for (int x = 1; f(1, x, x, x) <= MAX; x++)
-    for (int y = x; f(1, x, y, y) <= MAX; y++)
-      for (int z = y; f(1, x, y, z) <= MAX; z++)
-        for (int t = 1; f(t, x, y, z) <= MAX; t++)
-          cache[f(t, x, y, z)]++;
+        for (int x = 1; f(1, x, x, x) <= MAX; x++)
+                for (int y = x; f(1, x, y, y) <= MAX; y++)
+                        for (int z = y; f(1, x, y, z) <= MAX; z++)
+                                for (int t = 1; f(t, x, y, z) <= MAX; t++)
+                                        cache[f(t, x, y, z)]++;
 
-  for (int i = 1; i <= 100000; i++)
-    if (cache[i] == 1000)
-      printf("%d %d\n", i, cache[i]);
+        for (int i = 1; i <= 100000; i++)
+                if (cache[i] == 1000)
+                        printf("%d %d\n", i, cache[i]);
 
-  return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
