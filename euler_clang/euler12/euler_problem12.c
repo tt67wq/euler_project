374f75be5d1fb608a77266c58d6989f4a4ff070f
tt67wq <807643203@qq.com>
2018-07-17 12:23:53 +0800

    Who has two thumbs and remembers the rudiments of his linear algebra courses?  Apparently, this guy.

--- euler_clang/euler12/euler_problem12.c
+++ euler_clang/euler12/euler_problem12.c
@@ -20,6 +20,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define KEY_MAX_LENGTH (256)
+#define KEY_PREFIX ("fac")
+#define KEY_COUNT (1024 * 1024)
+
+typedef struct data_struct_s {
+        char key_string[KEY_MAX_LENGTH];
+        int number;
+} data_struct_t;
+
 // 质因数分解
 void factorize(int num, int *facs, int *len) {
         int i, j = 0;
@@ -39,9 +48,35 @@ int main() {
         scanf("%d", &num);
         int *facs = (int *)malloc(500 * sizeof(int));
         int len;
+        int error;
         factorize(num, facs, &len);
-        hmap_t hmap = hashmap_create();
-        for (int i = 0; i < len; i++) {
+        int nums[len];
+        map_t hmap = hashmap_new();
+        for (int i = 0; i <= len; i++) {
+                char key[KEY_MAX_LENGTH];
+                snprintf(key, KEY_MAX_LENGTH, "%s%d", KEY_PREFIX, facs[i]);
+                data_struct_t *value;
+
+                error = hashmap_get(hmap, key, (void **)(&value));
+                if (error == MAP_MISSING) {
+                        value = malloc(sizeof(data_struct_t));
+                        snprintf(value->key_string, KEY_MAX_LENGTH, "%s%d", KEY_PREFIX, facs[i]);
+                        value->number = 1;
+                        hashmap_put(hmap, value->key_string, value);
+                        nums[i] = facs[i];
+                } else {
+                        value->number += 1;
+                        hashmap_put(hmap, value->key_string, value);
+                }
+        }
+
+        for (int i = 0; i <= len; i++) {
+                char key[KEY_MAX_LENGTH];
+                data_struct_t *value;
+                snprintf(key, KEY_MAX_LENGTH, "%s%d", KEY_PREFIX, nums[i]);
+                error = hashmap_get(hmap, key, (void **)(&value));
+                if (error == MAP_OK)
+                        printf("%d => %d\n", nums[i], value->number);
         }
 
         return 0;
--- euler_clang/euler12/hashmap.h
+++ euler_clang/euler12/hashmap.h
@@ -1,102 +1,81 @@
 /*
- * =====================================================================================
+ * Generic hashmap manipulation functions
  *
- *       Filename:  hashmap.h
+ * Originally by Elliot C Back - http://elliottback.com/wp/hashmap-implementation-in-c/
  *
- *    Description:  哈希表
- *
- *        Version:  1.0
- *        Created:  2018-07-16
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  wanqiang
- *
- * =====================================================================================
+ * Modified by Pete Warden to fix a serious performance problem, support strings as keys
+ * and removed thread synchronization - http://petewarden.typepad.com
  */
+#ifndef __HASHMAP_H__
+#define __HASHMAP_H__
 
-// https://blog.csdn.net/ubuntu64fan/article/details/7704686
+#define MAP_MISSING -3  /* No such element */
+#define MAP_FULL -2 	/* Hashmap is full */
+#define MAP_OMEM -1 	/* Out of Memory */
+#define MAP_OK 0 	/* OK */
 
-#ifndef _HASHMAP_H_INCLUDED
-#define _HASHMAP_H_INCLUDED
-
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
-#define HMAP_E_KEYUSED (-5)  /* Key already existed */
-#define HMAP_E_OUTMEM (-4)   /* Out of Memory */
-#define HMAP_E_NOTFOUND (-3) /* No such element */
-#define HMAP_E_OVERFLOW (-2) /* Hashmap is full */
-#define HMAP_E_FAIL (-1)     /* Hashmap api fail */
-#define HMAP_S_OK (0)        /* Success */
-
-/**
- * void_ptr is a pointer. This allows you to put arbitrary structures in the hashmap.
+/*
+ * any_t is a pointer.  This allows you to put arbitrary structures in
+ * the hashmap.
  */
-typedef void *void_ptr;
+typedef void *any_t;
 
-/**
- * hmap_t is a pointer to an internally maintained data structure.
- * Clients of this package do not need to know how hashmaps are
- * represented. They see and manipulate only hmap_t's.
- */
-typedef void_ptr hmap_t;
-
-/**
- * hmap_callback_func is a pointer to a function that can take two void_ptr arguments
+/*
+ * PFany is a pointer to a function that can take two any_t arguments
  * and return an integer. Returns status code..
  */
-typedef int (*hmap_callback_func)(void_ptr, void_ptr);
+typedef int (*PFany)(any_t, any_t);
 
-/**
- * prototype for map element type
+/*
+ * map_t is a pointer to an internally maintained data structure.
+ * Clients of this package do not need to know how hashmaps are
+ * represented.  They see and manipulate only map_t's.
  */
-typedef struct _hmap_pair_t {
-        char *key;
-        void_ptr data;
-} hmap_pair_t;
+typedef any_t map_t;
 
-/**
+/*
  * Return an empty hashmap. Returns NULL if empty.
- */
-extern hmap_t hashmap_create();
+*/
+extern map_t hashmap_new();
 
-/**
- * Iteratively call fn with argument (value, arg) for each element data
- * in the hashmap. The function returns anything other than HMAP_S_OK
- * the traversal is terminated. fn must not modify any hashmap functions.
+/*
+ * Iteratively call f with argument (item, data) for
+ * each element data in the hashmap. The function must
+ * return a map status code. If it returns anything other
+ * than MAP_OK the traversal is terminated. f must
+ * not reenter any hashmap functions, or deadlock may arise.
  */
-extern int hashmap_iterate(hmap_t in, hmap_callback_func fnIterValue, void_ptr arg);
+extern int hashmap_iterate(map_t in, PFany f, any_t item);
 
-/**
- * Add an element to the hashmap.
- * Return HMAP_S_OK, HMAP_E_KEYUSED or HMAP_E_OUTMEM.
+/*
+ * Add an element to the hashmap. Return MAP_OK or MAP_OMEM.
  */
-extern int hashmap_put(hmap_t in, char *key, void_ptr elem);
+extern int hashmap_put(map_t in, char* key, any_t value);
 
-/**
- * Get an element from the hashmap. Return HMAP_S_OK or HMAP_E_NOTFOUND.
+/*
+ * Get an element from the hashmap. Return MAP_OK or MAP_MISSING.
  */
-extern int hashmap_get(hmap_t in, const char *key, void_ptr *elem);
+extern int hashmap_get(map_t in, char* key, any_t *arg);
 
-/**
- * Remove an element from the hashmap. Return HMAP_S_OK or HMAP_E_NOTFOUND.
+/*
+ * Remove an element from the hashmap. Return MAP_OK or MAP_MISSING.
  */
-extern int hashmap_remove(hmap_t in, char *key, void_ptr *outValue);
+extern int hashmap_remove(map_t in, char* key);
 
-/**
+/*
+ * Get any element. Return MAP_OK or MAP_MISSING.
+ * remove - should the element be removed from the hashmap
+ */
+extern int hashmap_get_one(map_t in, any_t *arg, int remove);
+
+/*
  * Free the hashmap
  */
-extern void hashmap_destroy(hmap_t in, hmap_callback_func fnFreeValue, void_ptr arg);
+extern void hashmap_free(map_t in);
 
-/**
+/*
  * Get the current size of a hashmap
  */
-extern int hashmap_size(hmap_t in);
+extern int hashmap_length(map_t in);
 
-#if defined(__cplusplus)
-}
-#endif
-
-#endif /* _HASHMAP_H_INCLUDED */
+#endif __HASHMAP_H__
