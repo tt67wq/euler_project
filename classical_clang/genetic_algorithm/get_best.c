eb617988b4c42d937b869790c9f5918314baf64b
tt67wq <807643203@qq.com>
2018-07-19 16:02:04 +0800

    This is a basic implementation that works.

--- classical_clang/genetic_algorithm/get_best.c
+++ classical_clang/genetic_algorithm/get_best.c
@@ -37,6 +37,7 @@ double fitness_prob[sizepop];    // 个体被选中的概率
 double bestfitness[maxgen];      // 每一代的最优值
 double gbest_pos[lenchrom];      // 最优值的位置
 double gbest;                    // 最优值
+double average_best[maxgen + 1]; // 每一代平均最优值
 int gbest_index;                 // 最优值下标
 
 // 目标函数
@@ -66,7 +67,7 @@ double *min(double *fitness) {
         for (int i = 1; i < sizepop; i++) {
                 if (*(fitness + i) < min_fit) {
                         min_fit = *(fitness + i);
-                        minx_index = i;
+                        min_index = i;
                 }
         }
         arr[0] = min_index;
@@ -115,7 +116,7 @@ void select(double chrom[sizepop][lenchrom]) {
         for (int i = 0; i < sizepop; i++) {
                 for (int j = 0; j < lenchrom; j++)
                         chrom[i][j] = chrom[index[i]][j];
-                fitness[i] = fitness[index[j]];
+                fitness[i] = fitness[index[i]];
         }
 }
 
@@ -142,7 +143,7 @@ void cross(double chrom[sizepop][lenchrom]) {
                         int pos = (int)(pick * sizepop);
                         while (pos > lenchrom - 1) {
                                 double pick = ((double)rand()) / RAND_MAX;
-                                int pos = (int)(pick * lenchrom);
+                                pos = (int)(pick * lenchrom);
                         }
                         // 开始交叉
                         double r = ((double)rand()) / RAND_MAX;
@@ -155,4 +156,77 @@ void cross(double chrom[sizepop][lenchrom]) {
                                 flag = 1; // 有效交叉
                 }
         }
+}
+
+// 变异
+void mutation(double chrom[sizepop][lenchrom]) {
+        for (int i = 0; i < sizepop; i++) {
+                double pick = ((double)rand()) / RAND_MAX;
+                int choice = (int)(pick * sizepop);
+                while (choice > sizepop - 1) {
+                        double pick = ((double)rand()) / RAND_MAX;
+                        choice = (int)(pick * sizepop); // 下标越界处理
+                }
+                pick = ((double)rand()) / RAND_MAX;
+                if (pick > pmutation)
+                        continue;
+                pick = ((double)rand()) / RAND_MAX;
+                int pos = (int)(pick * lenchrom);
+                while (pos > lenchrom - 1) {
+                        pick = ((double)rand()) / RAND_MAX;
+                        pos = (int)(pick * lenchrom);
+                }
+                double v = chrom[i][pos];
+                double v1 = v - bound_up;
+                double v2 = bound_down - v;
+
+                double r = ((double)rand()) / RAND_MAX;
+                double r1 = ((double)rand()) / RAND_MAX;
+
+                if (r >= 0.5)
+                        chrom[i][pos] = v - v1 * r1 * (1 - ((double)i) / maxgen) * (1 - ((double)i) / maxgen);
+                else
+                        chrom[i][pos] = v + v2 * r1 * (1 - ((double)i) / maxgen) * (1 - ((double)i) / maxgen); // 注意这里写法是不会越界的，所以只用一次变异就可以了
+        }
+}
+
+int main(void) {
+        time_t start, finish;
+        start = clock();             // 程序开始计时
+        srand((unsigned)time(NULL)); // 初始化随机数种子
+        init_chrom();                // 初始化种群
+        double *best_fit_index = min(fitness);
+        int best_index = (int)(*best_fit_index);
+        gbest = *(best_fit_index + 1); // 最优值
+        gbest_index = 0;
+        average_best[0] = sum(fitness) / sizepop; //初始平均最优值
+        for (int i = 0; i < lenchrom; i++)
+                gbest_pos[i] = chrom[best_index][i];
+        // 进化开始
+        for (int i = 0; i < maxgen; i++) {
+                select(chrom);   // 选择
+                cross(chrom);    //交叉
+                mutation(chrom); //变异
+                for (int j = 0; j < sizepop; j++) {
+                        fitness[j] = fit_func(chrom[j]);
+                }
+                double sum_fit = sum(fitness);
+                average_best[i + 1] = sum_fit / sizepop; // 平均最优值
+                double *arr = min(fitness);
+                double new_best = *(arr + 1);
+                int new_best_index = (int)(*arr); // 新最优值序号
+                if (new_best < gbest) {
+                        gbest = new_best;
+                        for (int j = 0; j < lenchrom; j++) {
+                                gbest_pos[j] = chrom[new_best_index][j];
+                        }
+                        gbest_index = i + 1;
+                }
+        }
+        finish = clock(); // 程序计算结束
+        double duration = ((double)(finish - start)) / CLOCKS_PER_SEC;
+        printf("程序计算耗时:%lf秒\n", duration);
+        printf("遗传算法进化了%d次，最优值为:%lf,最优值在第%d代取得,此代的平均最优值为%lf.\n", maxgen, gbest, gbest_index, average_best[gbest_index]);
+        printf("取得最优值的地方为(%lf,%lf,%lf,%lf,%lf).\n", gbest_pos[0], gbest_pos[1], gbest_pos[2], gbest_pos[3], gbest_pos[4]);
+        return 0;
 }
