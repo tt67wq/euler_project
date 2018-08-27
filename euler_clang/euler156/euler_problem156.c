ec21d29820f661b4a55d26e01fc3899982b82ac2
tt67wq <807643203@qq.com>
2018-08-27 12:58:59 +0800

    Replace all whitespaces with tabs.

--- euler_clang/euler156/euler_problem156.c
+++ euler_clang/euler156/euler_problem156.c
@@ -21,12 +21,32 @@
 #include <time.h>
 
 typedef unsigned long long ull;
+ull ten_base_count(int, int, int);
 
 void count_digits(ull num, ull *counter) {
         while (num > 0) {
                 counter[num % 10]++;
                 num /= 10;
         }
+}
+
+ull count_digits2(ull num, int base) {
+        ull tmp = num;
+        int p = 0;
+        ull count = 0;
+        while (tmp > 0) {
+                tmp /= 10;
+                p++;
+        }
+        while (p >= 0) {
+                ull n = (ull)pow(10, p);
+                count += ten_base_count((int)(num / n), p, base);
+                if ((int)(num / n) == base)
+                        count += num % n;
+                num %= n;
+                p--;
+        }
+        return count;
 }
 
 ull ten_base_count(int m, int n, int b) {
@@ -46,19 +66,25 @@ ull ten_base_count(int m, int n, int b) {
 }
 
 int main() {
-        ull counter[10] = {0};
-        for (ull i = 1; i < 1000000; i++) {
-                count_digits(i, counter);
-                /* for (int j = 1; j < 10; j++) */
-                /*         if (counter[j] == i) */
-                /*                 printf("f(%llu, %d) = %llu\n", i, j, i); */
-                if (i % 100000 == 0)
-                        for (int j = 1; j < 10; j++) {
-                                printf("f(%llu, %d) = %llu\n", i, j, counter[j]);
-                                printf("f(%llu, %d) = %llu\n", i, j,
-                                       ten_base_count(i / 100000, 5, j));
-                        }
+        /* ull counter[10] = {0}; */
+        /* for (ull i = 1; i < 1000000; i++) { */
+        /*         count_digits(i, counter); */
+        /*         for (int j = 1; j < 10; j++) */
+        /*                 if (counter[j] == i) { */
+        /*                         printf("f(%llu, %d) = %llu\n", i, j, i); */
+        /*                 } */
+        /* } */
+        for (ull i = 1; i < 10000000000; i++) {
+                ull tmp = count_digits2(i, 1);
+                if (tmp == i)
+                        printf("f(%llu, %d) = %llu\n", i, 1, tmp);
         }
+
+        /* for (int j = 1; j < 10; j++) { */
+        /*         ull tmp = count_digits2(i, j); */
+        /*         if (tmp == i) */
+        /*                 printf("f(%llu, %d) = %llu\n", i, j, tmp); */
+        /* } */
 
         return 0;
 }
