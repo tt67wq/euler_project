cb76d44afcff6569f864dfb5d449c1d27f994954
tt67wq <807643203@qq.com>
2018-08-27 10:36:25 +0800

    It'd be nice if type errors caused the compiler to issue a type error

--- euler_clang/euler156/euler_problem156.c
+++ euler_clang/euler156/euler_problem156.c
@@ -15,6 +15,7 @@
  * =====================================================================================
  */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -28,28 +29,35 @@ void count_digits(ull num, ull *counter) {
         }
 }
 
+ull ten_base_count(int m, int n, int b) {
+        if (m == 1) {
+                if (b != 1)
+                        return pow(10, n - 1) * n;
+                else
+                        return pow(10, n - 1) * n + 1;
+        } else {
+                if (m > b)
+                        return pow(10, n - 1) * n * m + pow(10, n);
+                else if (m == b)
+                        return pow(10, n - 1) * n * m + 1;
+                else
+                        return pow(10, n - 1) * n * m;
+        }
+}
+
 int main() {
-        ull sum = 0;
         ull counter[10] = {0};
         for (ull i = 1; i < 1000000; i++) {
                 count_digits(i, counter);
-                /* for (int j = 1; j < 10; j++) { */
-                /*         if (counter[j] == i) { */
+                /* for (int j = 1; j < 10; j++) */
+                /*         if (counter[j] == i) */
                 /*                 printf("f(%llu, %d) = %llu\n", i, j, i); */
-                /*                 sum += i; */
-                /*         } */
-                /* } */
-                /* if (i == 9999999 || i == 999999 || i == 99999 || i == 9999 || i == 999 || */
-                /*     i == 99 || i == 9) { */
-                /*         /\* for (int j = 1; j < 10; j++) *\/ */
-                /*         printf("f(%llu, %d) = %llu\n", i, 1, counter[1]); */
-                /* } */
-
                 if (i % 100000 == 0)
-                        printf("f(%llu, %d) = %llu\n", i, 1, counter[1]);
-
-                /* if (i == 221902 || i == 21902 || i == 200000) */
-                /*         printf("f(%llu, %d) = %llu\n", i, 7, counter[7]); */
+                        for (int j = 1; j < 10; j++) {
+                                printf("f(%llu, %d) = %llu\n", i, j, counter[j]);
+                                printf("f(%llu, %d) = %llu\n", i, j,
+                                       ten_base_count(i / 100000, 5, j));
+                        }
         }
 
         return 0;
--- euler_elixir/euler_problem156.ex
+++ euler_elixir/euler_problem156.ex
@@ -2,7 +2,33 @@ defmodule Euler156 do
   @moduledoc """
   https://projecteuler.net/problem=156
   """
-  def count_digit([h | t], d) when h != d do
-    count_digit(t, d) + count([h], d)
-  end
+  def digit_count(10, 1), do: 2
+  def digit_count(10, _), do: 1
+
+  def digit_count(100, 1), do: 21
+  def digit_count(100, _), do: 20
+
+  def digit_count(1000, 1), do: 301
+  def digit_count(1000, _), do: 300
+
+  def digit_count(10000, 1), do: 4001
+  def digit_count(10000, _), do: 4000
+
+  def digit_count(100_000, 1), do: 50001
+  def digit_count(100_000, _), do: 50000
+
+  def digit_count(1_000_000, 1), do: 600_001
+  def digit_count(1_000_000, _), do: 600_000
+
+  def digit_count(10_000_000, 1), do: 7_000_001
+  def digit_count(10_000_000, _), do: 7_000_000
+
+  def digit_count(100_000_000, 1), do: 80_000_001
+  def digit_count(100_000_000, _), do: 80_000_000
+
+  def digit_count(1_000_000_000, 1), do: 900_000_001
+  def digit_count(1_000_000_000, _), do: 900_000_000
+
+  def digit_count(10_000_000_000, 1), do: 10_000_000_001
+  def digit_count(10_000_000_000, _), do: 10_000_000_000
 end
