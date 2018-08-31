40bfe300cd4de3a3c7646061f5f59e41eff93901
tt67wq <807643203@qq.com>
2018-08-31 10:05:54 +0800

    making this thing actually usable.

---
+++ euler_clang/euler157/euler_problem157.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  euler_problem157.c
+ *
+ *    Description:  欧拉计划157题
+ *
+ *        Version:  1.0
+ *        Created:  2018-08-30
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  fangyuan
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
