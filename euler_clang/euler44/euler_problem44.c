ce18d9236178ad9edf4dc5cfda1f6303d828e5e2
tt67wq <807643203@qq.com>
2018-09-27 18:05:24 +0800

    this should fix it

---
+++ euler_clang/euler44/euler_problem44.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  euler_problem44.c
+ *
+ *    Description:  欧拉计划44题
+ *
+ *        Version:  1.0
+ *        Created:  2018-09-27
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  fangyuan
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int pentagon(int index) { return n * (3 * n - 1) / 2; }
+
+int main() {
+        clock_t begin = clock();
+        int ps[10000] = {0};
+        int acc[10000] = {0};
+        int index = 1;
+
+        while (1) {
+                int p = pentagon(index);
+                while (1) {
+                        int i = 0;
+                }
+        }
+        return 0;
+}
