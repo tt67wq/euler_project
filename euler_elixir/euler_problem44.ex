1a21eb80d43362f8b3753eae50c7f8c4157b158e
tt67wq <807643203@qq.com>
2018-09-27 14:59:09 +0800

    So my boss wanted this button ...

--- euler_elixir/euler_problem44.ex
+++ euler_elixir/euler_problem44.ex
@@ -5,17 +5,26 @@ defmodule PentagonNumbers do
   It can be seen that P4 + P7 = 22 + 70 = 92 = P8. However, their difference, 70 − 22 = 48, is not pentagonal.
   Find the pair of pentagonal numbers, Pj and Pk, for which their sum and difference are pentagonal and D = |Pk − Pj| is minimised; what is the value of D?
   """
+  defp now(), do: :os.system_time(:milli_seconds)
 
   @spec pentagon(Integer) :: Integer
   def pentagon(n), do: div(n * (3 * n - 1), 2)
 
-  def find(), do: find(1, [], [])
-  defp find(index, [], acc) do
-    nacc = [pentagon(index+1)|acc]
-    find(index+1, Enum.reverse(nacc), nacc)
+  def run() do
+    start = now()
+    res = find(1, [], [])
+    timeuse = now() - start
+    IO.puts("result ==> #{res}, timeuse ==> #{timeuse} milliseconds")
   end
-  defp find(index, [h|t], acc) do
+
+  defp find(index, [], acc) do
+    nacc = [pentagon(index + 1) | acc]
+    find(index + 1, Enum.reverse(nacc), nacc)
+  end
+
+  defp find(index, [h | t], acc) do
     p = pentagon(index)
+
     cond do
       acc |> Enum.member?(p - h) and acc |> Enum.member?(p - h * 2) -> p - h * 2
       :else -> find(index, t, acc)
