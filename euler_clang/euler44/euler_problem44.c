06c706adce44a1bde748a9fdeb22ff0a87436b1c
tt67wq <807643203@qq.com>
2018-09-28 10:45:27 +0800

    that's all folks

--- euler_clang/euler44/euler_problem44.c
+++ euler_clang/euler44/euler_problem44.c
@@ -19,19 +19,51 @@
 #include <stdlib.h>
 #include <time.h>
 
-int pentagon(int index) { return n * (3 * n - 1) / 2; }
+int pentagon(int n) { return n * (3 * n - 1) / 2; }
+
+// 二分搜索
+int binary_search(int target, int *list, int start, int end) {
+        int mid = list[(start + end) / 2];
+        if (mid == list[start] || mid == list[end])
+                return 0;
+        if (target == mid)
+                return (start + end) / 2;
+        if (target > mid)
+                return binary_search(target, list, (start + end) / 2, end);
+        if (target < mid)
+                return binary_search(target, list, start, (start + end) / 2);
+        return 0;
+}
+
+int check(int index, int *acc) {
+        int p1 = pentagon(index);
+        int i = index - 1;
+        while (i > 0) {
+                int p2 = pentagon(i);
+                if (binary_search(p1 - p2, acc, 0, index - 1) &&
+                    binary_search(p1 - 2 * p2, acc, 0, index - 1))
+                        return p1 - p2 * 2;
+                i--;
+        }
+        return 0;
+}
 
 int main() {
         clock_t begin = clock();
-        int ps[10000] = {0};
         int acc[10000] = {0};
         int index = 1;
-
+        int res = 0;
         while (1) {
-                int p = pentagon(index);
-                while (1) {
-                        int i = 0;
-                }
+                if ((res = check(index, acc)) > 0)
+                        break;
+
+                // not satisfy
+                acc[index] = pentagon(index);
+                index++;
         }
+
+        clock_t end = clock();
+        double timespent = end - begin;
+        printf("result => %d time use => %lfs\n", res, (double)(timespent / CLOCKS_PER_SEC));
         return 0;
 }
