941d1f030f12d53dd860c0fc05e44ab5f21fe2bb
tt67wq <807643203@qq.com>
2018-11-13 18:06:01 +0800

    Obligatory placeholder fixup message

--- euler_elixir/euler161.ex
+++ euler_elixir/euler161.ex
@@ -2,13 +2,14 @@ defmodule Euler161 do
   @moduledoc """
   https://projecteuler.net/problem=161
   """
-
-  @triomino1 %{0 => [1, 1], 1 => [1, 0]}
-  @triomino2 %{0 => [1, 1], 1 => [0, 1]}
-  @triomino3 %{0 => [1, 0], 1 => [1, 1]}
-  @triomino4 %{0 => [0, 1], 1 => [1, 1]}
-  @triomino5 %{0 => [1, 0], 1 => [1, 0], 2 => [1, 0]}
-  @triomino6 %{0 => [1, 1, 1]}
+  @triominos [
+    [{0, 0}, {1, 0}, {0, 1}],
+    [{0, 0}, {1, 0}, {1, 1}],
+    [{0, 0}, {0, 1}, {1, 1}],
+    [{0, 0}, {1, 0}, {1, -1}],
+    [{0, 0}, {0, 1}, {0, 2}],
+    [{0, 0}, {1, 0}, {2, 0}]
+  ]
 
   def now(), do: :os.system_time(:milli_seconds)
 
@@ -18,7 +19,8 @@ defmodule Euler161 do
   采用先填左侧，先填底部的原则避免重复
   每个阶段要过滤掉不合格的状态
   """
-  def state_iter(state) do
+  def state_iter(state, index) do
+    # 从6种积木中选一个塞进状态机，按照先左先下的原则
   end
 
   @doc """
@@ -30,18 +32,13 @@ defmodule Euler161 do
   def run() do
     {:ok, pid} = Task.start_link(fn -> loop_accept([]) end)
 
-    init_state = %{
-      8 => [0, 0],
-      7 => [0, 0],
-      6 => [0, 0],
-      5 => [0, 0],
-      4 => [0, 0],
-      3 => [0, 0],
-      2 => [0, 0],
-      1 => [0, 0],
-      0 => [0, 0]
-    }
+    # 初始状态
+    init_state =
+      for x <- 0..1,
+          y <- 0..8 do
+        {x, y, 0}
+      end
 
-    state_iter(init_state)
+    state_iter(init_state, 0)
   end
 end
