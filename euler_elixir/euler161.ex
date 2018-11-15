44e1f58b03c55dbc974a038f488f1d1ff20de598
tt67wq <807643203@qq.com>
2018-11-15 13:30:50 +0800

    ??! what the ...

--- euler_elixir/euler161.ex
+++ euler_elixir/euler161.ex
@@ -16,57 +16,52 @@ defmodule Euler161 do
     [{0, 0}, {1, 0}, {2, 0}]
   ]
 
-  @xlimit 3
-  @ylimit 8
-  @ilimit 12
-
   def now(), do: :os.system_time(:milli_seconds)
 
   @doc """
   状态机变换
   问题核心
-  采用先填左侧，先填底部的原则避免重复
+  采用从下至上，从左至右的原则填错避免重复
   每个阶段要过滤掉不合格的状态
   """
-  def state_iter(_state, index, pid) when index > @ilimit, do: send(pid, {:ok})
+  def state_iter(state, pid) do
+    w = Map.get(state, :wide)
+    l = Map.get(state, :length)
 
-  def state_iter(state, index, pid) do
-    # 从左下开始查找
-    {flag, a, b} = position_find(state, 0, 0)
+    {flag, a, b} = position_find(state, w, l, 0, 0)
 
     case flag do
       :error ->
-        # finish
-        send(pid, {:ok})
+        send(pid, {:ok, 1})
 
       :ok ->
         # 塞入积木
         @triominos
-        |> Enum.map(fn tr -> add_triominos(state, {a, b}, tr) end)
+        |> Enum.map(fn tr -> add_triominos(state, w, l, {a, b}, tr) end)
         |> Enum.filter(fn {flag, _} -> flag == :ok end)
-        |> Enum.each(fn {_, nst} -> state_iter(nst, index + 1, pid) end)
+        |> Enum.each(fn {_, nst} -> state_iter(nst, pid) end)
     end
   end
 
-  defp position_find(_state, _a, b) when b > @ylimit, do: {:error, 0, 0}
-  defp position_find(state, a, b) when a > @xlimit, do: position_find(state, 0, b + 1)
+  defp position_find(_state, w, _l, a, _b) when a > w, do: {:error, 0, 0}
+  defp position_find(state, w, l, a, b) when b > l, do: position_find(state, w, l, a + 1, 0)
 
-  defp position_find(state, a, b) do
+  defp position_find(state, w, l, a, b) do
     case Map.get(state, {a, b}, -1) do
       0 -> {:ok, a, b}
-      1 -> position_find(state, a + 1, b)
+      1 -> position_find(state, w, l, a, b + 1)
     end
   end
 
   # 在{a,b}点中塞入积木
-  defp add_triominos(state, {a, b}, [{x0, y0}, {x1, y1}, {x2, y2}]) do
+  defp add_triominos(state, w, l, {a, b}, [{x0, y0}, {x1, y1}, {x2, y2}]) do
     # 三个点都必须在框内
     dots = [{a + x0, b + y0}, {a + x1, b + y1}, {a + x2, b + y2}]
 
     c =
       dots
-      |> Enum.filter(fn {x, _y} -> x <= @xlimit and x >= 0 end)
-      |> Enum.filter(fn {_x, y} -> y <= @ylimit and y >= 0 end)
+      |> Enum.filter(fn {x, _y} -> x <= w and x >= 0 end)
+      |> Enum.filter(fn {_x, y} -> y <= l and y >= 0 end)
       |> Enum.count()
 
     case c do
@@ -94,32 +89,51 @@ defmodule Euler161 do
   """
   def loop_accept(acc) do
     receive do
-      {:ok} ->
-        loop_accept(acc + 1)
+      {:ok, n} ->
+        loop_accept(acc + n)
 
-      {:finish} ->
-        IO.puts(acc)
+      {:finish, pid} ->
+        send(pid, {:ok, acc})
 
       {:error} ->
         loop_accept(acc)
     end
   end
 
-  def run() do
-    start = now()
+  def calculate_path(state) do
     {:ok, pid} = Task.start_link(fn -> loop_accept(0) end)
 
+    state_iter(state, pid)
+    send(pid, {:finish, self()})
+
+    receive do
+      {:ok, res} ->
+        res
+    end
+  end
+
+  defp state_code(state), do
+    
+  end
+
+  def run() do
+    # :ets.new(:euler161, [:named_table])
+    # start = now()
+    # timeuse = now() - start
+    # IO.puts("timeuse => #{timeuse} milliseconds")
+
+
     # 初始状态
-    init_state =
-      for x <- 0..@xlimit,
-          y <- 0..@ylimit do
+    state =
+      for x <- 0..1,
+          y <- 0..8 do
         {x, y}
       end
       |> Enum.reduce(%{}, fn {x, y}, acc -> Map.put(acc, {x, y}, 0) end)
 
-    state_iter(init_state, 0, pid)
-    send(pid, {:finish})
-    timeuse = now() - start
-    IO.puts("timeuse => #{timeuse} milliseconds")
+    state
+    |> Map.put(:wide, 1)
+    |> Map.put(:length, 8)
+    |> calculate_path()
   end
 end
