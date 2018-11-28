dc0ad8406d7e82fe2b1d76e4fa71872ec9bc40d0
tt67wq <807643203@qq.com>
2018-11-28 11:35:25 +0800

    It's Working!

--- classical_clang/permutation/permutation.c
+++ classical_clang/permutation/permutation.c
@@ -19,15 +19,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-int array[] = {1, 2, 3, 4};
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
 
-#define N 4
-#define M 3
-int results[N] = {0}; //保存已经找到的结果前缀数组
-int results_end = 0;  //结果前缀数组有效数据下标
-int is_used[N] = {0}; //是否已经排列过了的标志
-
-void perm(int deep, int n) {
+void perm(int deep, int n, int *array, int *results, int *is_used) {
         int i;
         if (deep > n) // 越界递归结束
         {
@@ -37,7 +31,7 @@ void perm(int deep, int n) {
         if (deep == n) // 找到结果，打印，递归结束
         {
                 for (i = 0; i < n; i++) {
-                        printf("%d", results[i]);
+                        printf("%d ", results[i]);
                 }
                 printf("\n");
                 return;
@@ -47,13 +41,16 @@ void perm(int deep, int n) {
                 if (is_used[i] == 0) {
                         is_used[i] = 1;
                         results[deep] = array[i];
-                        perm(deep + 1, n);
+                        perm(deep + 1, n, array, results, is_used);
                         is_used[i] = 0;
                 }
         }
 }
 
 int main() {
-        perm(0, 4);
+        int array[4] = {1, 2, 3, 4};
+        int results[4] = {0};
+        int is_used[4] = {0};
+        perm(0, 4, array, results, is_used);
         return 0;
 }
---
+++ euler_clang/euler51/euler51.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  euler51.c
+ *
+ *    Description:  欧拉计划51题
+ *
+ *        Version:  1.0
+ *        Created:  2018-11-28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  fangyuan
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
