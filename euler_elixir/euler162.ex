49f6040f62949f3976f51f8244898420419c6983
tt67wq <807643203@qq.com>
2018-11-28 09:26:12 +0800

    FONDLED THE CODE

--- euler_elixir/euler162.ex
+++ euler_elixir/euler162.ex
@@ -13,23 +13,10 @@ defmodule Euler162 do
     result * result
   end
 
-  def contains(list), do: ct(list, {0, 0, 0})
+  def startswithA1(n), do: pow(16, n - 1) - pow(15, n - 1) * 2 + pow(14, n - 1)
 
-  defp ct([], {a, b, c}), do: a > 0 and b > 0 and c > 0
-
-  defp ct([h | t], {a, b, c}) do
-    case h do
-      0 -> ct(t, {a + 1, b, c})
-      1 -> ct(t, {a, b + 1, c})
-      10 -> ct(t, {a, b, c + 1})
-      _ -> ct(t, {a, b, c})
-    end
-  end
-
-  # ----------- test -------------
-
-  def startswith01(n), do: pow(16, n - 1) - pow(15, n - 1) * 2 + pow(14, n - 1)
-  def startswithx(n), do: calculate_n(n - 1) * startswith01(n - 1)
+  def startswithx(3), do: 0
+  def startswithx(n), do: calculate_n(n - 1) + startswithA1(n - 1)
 
   def calculate_n(3), do: 4
 
@@ -39,23 +26,26 @@ defmodule Euler162 do
         value
 
       [] ->
-        v = 2 * startswith01(n) + 13 * startswithx(n)
+        v = 2 * startswithA1(n) + 13 * startswithx(n)
         :ets.insert(:euler162, {n, v})
-	v
+        v
     end
   end
 
+  def now(), do: :os.system_time(:milli_seconds)
+
   def run() do
-    # for a <- 0..15,
-    #     b <- 0..15,
-    #     c <- 0..15,
-    #     d <- 0..15 do
-    #   [a, b, c, d]
-    # end
-    # |> Enum.filter(fn x -> contains(x) end)
-    # |> Enum.filter(fn [h | _t] -> h != 0 end)
-    # |> Enum.count()
+    start = now()
     :ets.new(:euler162, [:named_table])
-    calculate_n(3)
+
+    res =
+      3..16
+      |> Enum.map(fn x -> calculate_n(x) end)
+      |> Enum.sum()
+      |> Integer.to_string(16)
+
+    IO.puts(res)
+    timeuse = now() - start
+    IO.puts("timeuse => #{timeuse} milliseconds")
   end
 end
