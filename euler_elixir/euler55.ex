aa7ed2ad19ad460ff25d2aa76194d192d6bb53bf
tt67wq <807643203@qq.com>
2018-12-14 17:18:49 +0800

    "Get that shit outta my master."

--- euler_elixir/euler55.ex
+++ euler_elixir/euler55.ex
@@ -4,25 +4,20 @@ defmodule Euler55 do
   """
   @limit 50
 
-  @doc """
-  是否为回文数
-  """
   @spec palindrome?(Integer) :: boolean
   def palindrome?(number), do: number_reverse(number) == number
 
-  @doc """
-  数字调转
-  """
+
   @spec number_reverse(Integer) :: Integer
   def number_reverse(number), do: nr(number, 0)
   defp nr(0, acc), do: acc
   defp nr(num, acc), do: nr(div(num, 10), acc * 10 + rem(num, 10))
 
-  @doc """
-  数字和它的反转数字和
-  """
+
+  @spec reverse_sum(Integer) :: Integer
   def reverse_sum(number), do: number + number_reverse(number)
 
+  @spec lychrel?(Integer) :: boolean()
   def lychrel?(number), do: lychrel(number, 1)
   defp lychrel(_number, index) when index == @limit, do: true
 
@@ -35,6 +30,7 @@ defmodule Euler55 do
     end
   end
 
+  @spec now() :: Integer
   def now(), do: :os.system_time(:milli_seconds)
 
   def run() do
