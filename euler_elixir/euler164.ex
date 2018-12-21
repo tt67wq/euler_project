129c1921773bccc6ac1d6ab4afaa66a500b47a6f
tt67wq <807643203@qq.com>
2018-12-21 13:10:06 +0800

    It'd be nice if type errors caused the compiler to issue a type error

--- euler_elixir/euler164.ex
+++ euler_elixir/euler164.ex
@@ -3,38 +3,10 @@ defmodule Euler164 do
   https://projecteuler.net/problem=164
   """
 
-  require Logger
+  def now(), do: :os.system_time(:milli_seconds)
 
-  # defp merge([], acc), do: acc
-  # defp merge([h | t], []), do: merge(t, h)
-  # defp merge([[h, _, _] | t], acc), do: merge(t, [h | acc])
-
-  def get_unit_cluster(unit, _, 0), do: [[unit]]
-
-  def get_unit_cluster(unit, unit_map, deep) do
-    key = {unit, deep}
-
-    case :ets.lookup(:euler164, key) do
-      [{^key, v}] ->
-        v
-
-      [] ->
-        res =
-          get_unit_cluster(unit, unit_map, deep - 1)
-          |> Enum.map(fn [x | _t] = all -> {Enum.take(x, -2), all} end)
-          |> Enum.map(fn {base, all} -> {Map.get(unit_map, base), all} end)
-          |> Enum.reduce([], fn {es, all}, acc -> acc ++ Enum.map(es, fn e -> [e | all] end) end)
-
-        :ets.insert(:euler164, {key, res})
-        res
-    end
-  end
-
-  def start() do
-    :ets.new(:euler164, [:named_table])
-  end
-
-  def run(n) do
+  def run() do
+    start = now()
     cs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     units =
@@ -45,19 +17,54 @@ defmodule Euler164 do
       end
       |> Enum.filter(fn x -> Enum.sum(x) <= 9 end)
 
-    # IO.puts(Enum.count(units))
-
     mp_func = fn x, acc ->
+      Map.update(acc, Enum.take(x, 2), 1, &(&1 + 1))
+    end
+
+    mp_func2 = fn x, acc ->
       Map.update(acc, Enum.take(x, 2), [x], &[x | &1])
     end
 
-    unit_map = Enum.reduce(units, %{}, mp_func)
+    mp_func3 = fn x, acc ->
+      Map.update(acc, Enum.take(x, -2), 1, &(&1 + 1))
+    end
 
-    start_units = Enum.filter(units, fn [a, _, _] -> a != 0 end)
+    start_count_map = Enum.reduce(units, %{}, mp_func)
+    start_unit_map = Enum.reduce(units, %{}, mp_func2)
 
-    start_units
-    |> Enum.map(fn unit -> get_unit_cluster(unit, unit_map, n) end)
-    |> Enum.map(fn xs -> Enum.count(xs) end)
-    |> Enum.sum()
+    ends_count_map =
+      Enum.filter(units, fn [a, _, _] -> a != 0 end)
+      |> Enum.reduce(%{}, mp_func3)
+
+    res =
+      ends_iter(ends_count_map, start_count_map, start_unit_map, 0)
+      |> Map.to_list()
+      |> Enum.map(fn {unit, c} -> c * Map.get(start_count_map, unit) end)
+      |> Enum.sum()
+
+    IO.puts(res)
+
+    timeuse = now() - start
+    IO.puts("timeuse => #{timeuse} milliseconds")
+  end
+
+  defp ends_iter(ends_count_map, _, _, 16), do: ends_count_map
+
+  defp ends_iter(ends_count_map, starts_count_map, start_unit_map, index) do
+    new_ends_count_map =
+      Map.to_list(ends_count_map)
+      |> Enum.map(fn {unit, count} ->
+        Map.get(start_unit_map, unit)
+        |> Enum.reduce(%{}, fn x, acc ->
+          Map.update(acc, Enum.take(x, -2), count, &(&1 + count))
+        end)
+      end)
+      |> Enum.reduce(%{}, fn mp, acc ->
+        Map.merge(acc, mp, fn _k, v1, v2 ->
+          v1 + v2
+        end)
+      end)
+
+    ends_iter(new_ends_count_map, starts_count_map, start_unit_map, index + 1)
   end
 end
