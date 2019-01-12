d160667701c3c87b68e9b3cf6a2ffad355d0eadc
tt67wq <807643203@qq.com>
2019-01-12 17:59:15 +0800

    This solves it.

--- euler_elixir/euler167.ex
+++ euler_elixir/euler167.ex
@@ -7,114 +7,61 @@ defmodule Euler167 do
   """
   require Logger
 
-  @top 10000
-  @prefix_limit 20
-  @periodic_limit 500
+  @target 100_000_000_000
+  @top 2000
+  @periods [32, 26, 444, 1628, 5906, 80, 126_960, 380_882, 2_097_152]
+  @diffs [126, 126, 1778, 6510, 23622, 510, 507_842, 1_523_526, 8_388_606]
 
   # 硬算序列
-  # def ulam(a, b), do: ulam_iter(MapSet.new([a + b]), MapSet.new(), 0, [b, a])
+  def ulam(a, b), do: ulam_iter(MapSet.new([a + b]), MapSet.new(), 0, [b, a])
 
-  # defp ulam_iter(_, _, @top, acc), do: Enum.reverse(acc)
+  defp ulam_iter(_, _, @top, acc), do: Enum.reverse(acc)
 
-  # defp ulam_iter(set, jump_set, index, acc) do
-  #   next = Enum.min(set)
+  defp ulam_iter(set, jump_set, index, acc) do
+    next = Enum.min(set)
 
-  #   set = MapSet.delete(set, next)
+    set = MapSet.delete(set, next)
 
-  #   # 重复的加入jump_set
-  #   new_jump_set =
-  #     Enum.map(acc, fn x -> x + next end)
-  #     |> Enum.filter(fn x -> MapSet.member?(set, x) end)
-  #     |> Enum.filter(fn x -> x > next end)
-  #     |> Enum.reduce(jump_set, fn x, acc -> MapSet.put(acc, x) end)
+    # 重复的加入jump_set
+    new_jump_set =
+      Enum.map(acc, fn x -> x + next end)
+      |> Enum.filter(fn x -> MapSet.member?(set, x) end)
+      |> Enum.filter(fn x -> x > next end)
+      |> Enum.reduce(jump_set, fn x, acc -> MapSet.put(acc, x) end)
 
-  #   new_set =
-  #     Enum.map(acc, fn x -> x + next end)
-  #     |> Enum.reduce(set, fn x, acc -> MapSet.put(acc, x) end)
+    new_set =
+      Enum.map(acc, fn x -> x + next end)
+      |> Enum.reduce(set, fn x, acc -> MapSet.put(acc, x) end)
 
-  #   ulam_iter(MapSet.difference(new_set, new_jump_set), new_jump_set, index + 1, [next | acc])
-  # end
-
-  def ulam(a, b), do: ulam_iter(%{(a + b) => 1}, [b, a], 1)
-
-  defp ulam_iter(_, acc, @top), do: Enum.reverse(acc)
-
-  defp ulam_iter(mp, [h | _] = acc, index) do
-    # {next, 1} =
-    #   Map.to_list(mp)
-    #   |> Enum.filter(fn {_, x} -> x == 1 end)
-    #   |> Enum.min()
-    {new_mp, next} = search_next(mp, h)
-
-    # new_mp =
-    #   Map.keys(mp)
-    #   |> Enum.filter(fn x -> x <= next end)
-    #   |> Enum.reduce(mp, fn x, bcc -> Map.delete(bcc, x) end)
-
-    Enum.map(acc, fn x -> x + next end)
-    |> Enum.reduce(new_mp, fn x, bcc -> Map.update(bcc, x, 1, &(&1 + 1)) end)
-    |> ulam_iter([next | acc], index + 1)
-  end
-
-  def search_next(mp, from) do
-    case Map.get(mp, from) do
-      nil -> search_next(mp, from + 1)
-      1 -> {Map.delete(mp, from), from}
-      _ -> search_next(Map.delete(mp, from), from + 1)
-    end
-  end
-
-  # 计算序列差值
-  def consecutive_diff(list), do: cons_diff(list, [])
-  defp cons_diff([_], acc), do: acc
-  defp cons_diff([h1, h2 | t], acc), do: cons_diff([h2 | t], [h2 - h1 | acc])
-
-  # 寻找周期串 返回前缀部分 和 周期串
-  def search_periodic_part([_h | t] = list) do
-    case search(t, 1) do
-      {pl, periodic} ->
-        {Enum.take(list, pl), Enum.count(periodic), Enum.sum(periodic)}
-
-      _ ->
-        nil
-    end
-  end
-
-  defp search(_, @prefix_limit), do: nil
-
-  defp search([_h | t] = list, prefix_len) do
-    case sp(list, 10) do
-      nil -> search(t, prefix_len + 1)
-      {:ok, p} -> {prefix_len, p}
-    end
-  end
-
-  defp sp(_list, @periodic_limit), do: nil
-
-  defp sp(list, periodic_len) do
-    ps =
-      Enum.take(list, 5 * periodic_len)
-      |> Enum.chunk_every(periodic_len)
-
-    cond do
-      all_equal?(ps) -> {:ok, List.first(ps)}
-      :else -> sp(list, periodic_len + 1)
-    end
-  end
-
-  def all_equal?([h | t]) do
-    case Enum.find(t, fn x -> x != h end) do
-      nil -> true
-      _ -> false
-    end
+    ulam_iter(MapSet.difference(new_set, new_jump_set), new_jump_set, index + 1, [next | acc])
   end
 
   def now(), do: :os.system_time(:milli_seconds)
 
   def run() do
     start = now()
-    res = ulam(2, 5)
+
+    res =
+      Enum.zip([[5, 7, 9, 11, 13, 15, 17, 19, 21], @periods, @diffs])
+      |> Stream.map(fn {x, y, z} -> {ulam(2, x), y, z} end)
+      |> Stream.map(fn {x, y, z} -> iter(x, 0, y, z) end)
+      |> Enum.take(10)
+      |> Enum.sum()
+
     IO.inspect(res)
     IO.puts("timeuse => #{now() - start} milliseconds")
+  end
+
+  defp iter([h], index, period, diff), do: h + div(@target - index, period) * diff
+
+  defp iter([h | t], index, period, diff) do
+    case rem(@target - index, period) do
+      0 ->
+        # 刚好剩余整数个循环结
+        h + div(@target - index, period) * diff
+
+      _ ->
+        iter(t, index + 1, period, diff)
+    end
   end
 end
