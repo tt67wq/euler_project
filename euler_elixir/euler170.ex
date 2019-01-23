74018aac36f34e45c02cfae6daa18a46d62cecf2
tt67wq <807643203@qq.com>
2019-01-23 10:28:29 +0800

    WTF is this.

--- euler_elixir/euler170.ex
+++ euler_elixir/euler170.ex
@@ -5,6 +5,7 @@ defmodule Euler170 do
   require Integer
   require Logger
 
+  def num2digits(0), do: [0]
   def num2digits(num), do: digits(num, [])
   defp digits(0, acc), do: Enum.sort(acc)
   defp digits(num, acc), do: digits(div(num, 10), [rem(num, 10) | acc])
@@ -51,17 +52,38 @@ defmodule Euler170 do
     first ++ [next] ++ rest
   end
 
-  def test() do
-    permutations([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
-    |> Stream.map(fn x -> Enum.map(1..9, fn y -> Enum.split(x, y) end) end)
-    |> Enum.filter(fn x -> x end
-    )
+  defp check(a, b) do
+    1..100
+    |> Enum.filter(fn x -> rem(a, x) == 0 and rem(b, x) == 0 end)
+    |> Enum.map(fn x ->
+      {x, num2digits(div(a, x)), num2digits(div(b, x)), div(a, x), div(b, x)}
+    end)
+    |> Enum.filter(fn {x, y, z, _, _} ->
+      Enum.sort(y ++ z ++ num2digits(x)) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end)
+    |> Enum.map(fn {x, _, _, y, z} -> {x, y, z} end)
   end
 
-  def check(a, b) do
-    2..9
-    |> Enum.filter(fn x -> rem(a, x) == 0 and rem(b, x) == 0 end)
-    |> Enum.map(fn x -> {x, num2digits(div(a, x)), num2digits(div(b, x))} end)
-    |> Enum.filter(fn {x, y, z} -> Enum.sort([x | y ++ z]) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] end)
+  def now(), do: :os.system_time(:milli_seconds)
+
+  def run() do
+    start = now()
+
+    res =
+      permutations([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+      |> Stream.map(fn x -> Enum.map(1..9, fn y -> Enum.split(x, y) end) end)
+      |> Stream.map(fn list ->
+        Enum.filter(list, fn {a, b} -> check(digits2num(a), digits2num(b)) != [] end)
+      end)
+      |> Stream.filter(fn x -> x != [] end)
+      |> Stream.map(fn list ->
+        Enum.map(list, fn {a, b} ->
+          {digits2num(a), digits2num(b), check(digits2num(a), digits2num(b))}
+        end)
+      end)
+      |> Enum.take(1)
+
+    IO.inspect(res)
+    IO.puts("timeuse => #{now() - start} milliseconds")
   end
 end
