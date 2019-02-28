93938db48e32e599136d53013b0e29e95c3cb33c
tt67wq <807643203@qq.com>
2019-02-28 14:10:28 +0800

    This really should not take 19 minutes to build.

--- classical_clang/graph/kruskal.c
+++ classical_clang/graph/kruskal.c
@@ -55,89 +55,87 @@ void Kruskal(PGraph G) {
     m = Find(parent, G->edges[i]->end);
     if (n != m) {
       // 判断边的两个点是否在一个树里面
-      parent[n] = m; // 将n与m点连起来
-      printf("(%d, %d) %d\n", G->edges[i]->start, G->edges[i]->end,
-             G->edges[i]->weight);
-    }
-    if (IsCompeleted(parent)) // 判断是否已经是生成树
-      return;
-  }
+      parent[n] = m; // 将n与m点连起来printf("(%d, %d) %d\n", G->edges[i]->start, G->edges[i]->end, G->edges[i]->weight);
+                }
+                if (IsCompeleted(parent)) // 判断是否已经是生成树
+                        return;
+        }
 }
 
 int Find(int *parent, int f) {
-  while (parent[f] != -1)
-    f = parent[f];
-  return f;
+        while (parent[f] != -1)
+                f = parent[f];
+        return f;
 }
 
 int IsCompeleted(int *parent) {
-  int i;
-  int n = 0;
-  for (i = 0; i < MAXVEX; ++i) {
-    if (parent[i] != -1)
-      ++n;
-  }
-  if (n == MAXVEX - 1)
-    return 1;
-  else
-    return 0;
+        int i;
+        int n = 0;
+        for (i = 0; i < MAXVEX; ++i) {
+                if (parent[i] != -1)
+                        ++n;
+        }
+        if (n == MAXVEX - 1)
+                return 1;
+        else
+                return 0;
 }
 
 int main() {
-  PGraph G;
-  int i, j;
-  int t = 0;
-  G = malloc(sizeof(Graph));
-  G->edgenum = 10;
-  G->vexnum = 6;
-  G->vex = malloc(sizeof(int) * G->vexnum);
-  /* G->edges = malloc(sizeof(Edge) * G->edgenum); */
-  for (i = 0; i < G->edgenum; ++i)
-    G->edges[i] = malloc(sizeof(Edge));
-  printf("初始化图\n");
-  for (i = 0; i < G->vexnum; ++i)
-    G->vex[i] = i;
-  G->edges[0]->start = 0;
-  G->edges[0]->end = 2;
-  G->edges[0]->weight = 1;
+        PGraph G;
+        int i, j;
+        int t = 0;
+        G = malloc(sizeof(Graph));
+        G->edgenum = 10;
+        G->vexnum = 6;
+        G->vex = malloc(sizeof(int) * G->vexnum);
+        /* G->edges = malloc(sizeof(Edge) * G->edgenum); */
+        for (i = 0; i < G->edgenum; ++i)
+                G->edges[i] = malloc(sizeof(Edge));
+        printf("初始化图\n");
+        for (i = 0; i < G->vexnum; ++i)
+                G->vex[i] = i;
+        G->edges[0]->start = 0;
+        G->edges[0]->end = 2;
+        G->edges[0]->weight = 1;
 
-  G->edges[1]->start = 3;
-  G->edges[1]->end = 5;
-  G->edges[1]->weight = 2;
+        G->edges[1]->start = 3;
+        G->edges[1]->end = 5;
+        G->edges[1]->weight = 2;
 
-  G->edges[2]->start = 1;
-  G->edges[2]->end = 4;
-  G->edges[2]->weight = 3;
+        G->edges[2]->start = 1;
+        G->edges[2]->end = 4;
+        G->edges[2]->weight = 3;
 
-  G->edges[3]->start = 2;
-  G->edges[3]->end = 5;
-  G->edges[3]->weight = 4;
+        G->edges[3]->start = 2;
+        G->edges[3]->end = 5;
+        G->edges[3]->weight = 4;
 
-  G->edges[4]->start = 0;
-  G->edges[4]->end = 3;
-  G->edges[4]->weight = 5;
+        G->edges[4]->start = 0;
+        G->edges[4]->end = 3;
+        G->edges[4]->weight = 5;
 
-  G->edges[5]->start = 1;
-  G->edges[5]->end = 2;
-  G->edges[5]->weight = 5;
+        G->edges[5]->start = 1;
+        G->edges[5]->end = 2;
+        G->edges[5]->weight = 5;
 
-  G->edges[6]->start = 2;
-  G->edges[6]->end = 4;
-  G->edges[6]->weight = 5;
+        G->edges[6]->start = 2;
+        G->edges[6]->end = 4;
+        G->edges[6]->weight = 5;
 
-  G->edges[7]->start = 0;
-  G->edges[7]->end = 1;
-  G->edges[7]->weight = 6;
+        G->edges[7]->start = 0;
+        G->edges[7]->end = 1;
+        G->edges[7]->weight = 6;
 
-  G->edges[8]->start = 4;
-  G->edges[8]->end = 5;
-  G->edges[8]->weight = 6;
+        G->edges[8]->start = 4;
+        G->edges[8]->end = 5;
+        G->edges[8]->weight = 6;
 
-  G->edges[9]->start = 2;
-  G->edges[9]->end = 3;
-  G->edges[9]->weight = 7;
-  printf("初始化图完成\n");
+        G->edges[9]->start = 2;
+        G->edges[9]->end = 3;
+        G->edges[9]->weight = 7;
+        printf("初始化图完成\n");
 
-  Kruskal(G);
-  return 1;
+        Kruskal(G);
+        return 1;
 }
