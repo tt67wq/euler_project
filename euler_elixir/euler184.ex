727a482439654e09f7f9f43a08e20566b8076f64
tt67wq <807643203@qq.com>
2019-03-18 18:05:15 +0800

    Merging 'WIP: Do Not Merge This Branch' Into Master

--- euler_elixir/euler184.ex
+++ euler_elixir/euler184.ex
@@ -3,6 +3,7 @@ defmodule Euler184 do
   https://projecteuler.net/problem=184
   """
   require Integer
+  require Logger
 
   # 判断原点是否在点a,b,c组成的三角形内部
   def inner_origin({ax, ay}, {bx, by}, {cx, cy}) do
@@ -24,22 +25,41 @@ defmodule Euler184 do
     end
   end
 
+  # 向量差积
+  def vector_cross({x1, y1}, {x2, y2}), do: x1 * y2 - x2 * y1
+
+  def test2(n) do
+    points = get_points(n)
+
+    # group_points(points, %{})
+    # mp = group_points(points, %{})
+    # base_ps = Map.keys(mp)
+    around_point(points, 0)
+  end
+
+  defp around_point([_], acc), do: acc
+
+  defp around_point([h | t], acc) do
+    Enum.filter(t, fn x -> vector_cross(h, t) <= 0 end)
+    around_point(t, acc)
+  end
+
   def test(n) do
     points = get_points(n)
 
     group_points(points, %{})
-    # mp = group_points(points, %{})
-    # base_ps = Map.keys(mp)
+    mp = group_points(points, %{})
+    base_ps = Map.keys(mp)
 
-    # for a <- base_ps,
-    #     b <- base_ps,
-    #     c <- base_ps do
-    #   {a, b, c}
-    # end
-    # |> Enum.uniq_by(fn {a, b, c} -> Enum.sort([a, b, c]) end)
-    # |> Enum.filter(fn {a, b, c} -> inner_origin(a, b, c) end)
-    # |> Enum.map(fn {a, b, c} -> Map.fetch!(mp, a) * Map.fetch!(mp, b) * Map.fetch!(mp, c) end)
-    # |> Enum.sum
+    for a <- base_ps,
+        b <- base_ps,
+        c <- base_ps do
+      {a, b, c}
+    end
+    |> Enum.uniq_by(fn {a, b, c} -> Enum.sort([a, b, c]) end)
+    |> Enum.filter(fn {a, b, c} -> inner_origin(a, b, c) end)
+    |> Enum.map(fn {a, b, c} -> Map.fetch!(mp, a) * Map.fetch!(mp, b) * Map.fetch!(mp, c) end)
+    |> Enum.sum()
   end
 
   defp group_points([], acc), do: acc
