8fb58178ba4cdf168979a8300a09ff41cd13e568
tt67wq <807643203@qq.com>
2019-03-20 17:53:22 +0800

    someone fails and it isn't me

--- euler_elixir/euler184.ex
+++ euler_elixir/euler184.ex
@@ -23,74 +23,63 @@ defmodule Euler184 do
         x * x + y * y < n * n do
       {x, y}
     end
+    |> Enum.filter(fn x -> x != {0, 0} end)
   end
 
   # 向量差积
   def vector_cross({x1, y1}, {x2, y2}), do: x1 * y2 - x2 * y1
 
-  def test2(n) do
+  def test(n) do
     points = get_points(n)
 
+    pnum = Enum.count(points)
+    p1 = around_point(points, points, 0)
+
+    gp = group_points(points, %{})
+    Logger.info("#{inspect(gp)}")
     # group_points(points, %{})
-    # mp = group_points(points, %{})
-    # base_ps = Map.keys(mp)
-    around_point(points, points, 0)
+    p2 =
+      gp
+      |> Map.values()
+      |> Enum.map(fn x -> div(x, 2) * div(x, 2) * (pnum - x) + div(x * (x - 1) * (x - 2), 6) end)
+      |> Enum.sum()
+
+    Logger.info("#{p1}, #{p2}")
+
+    div(pnum * (pnum - 1) * (pnum - 2), 6) - div(p1, 2) - p2
   end
 
   defp around_point([], _, acc), do: acc
 
   defp around_point([h | t], ps, acc) do
     n =
-      Enum.filter(ps, fn x -> vector_cross(h, x) <= 0 end)
+      Enum.filter(ps, fn x -> vector_cross(x, h) < 0 end)
       |> Enum.count()
 
-    around_point(t, ps, acc + div(n * (n - 1), 2))
-  end
-
-  def test(n) do
-    points = get_points(n)
-
-    group_points(points, %{})
-    mp = group_points(points, %{})
-    base_ps = Map.keys(mp)
-
-    for a <- base_ps,
-        b <- base_ps,
-        c <- base_ps do
-      {a, b, c}
-    end
-    |> Enum.uniq_by(fn {a, b, c} -> Enum.sort([a, b, c]) end)
-    |> Enum.filter(fn {a, b, c} -> inner_origin(a, b, c) end)
-    |> Enum.map(fn {a, b, c} -> Map.fetch!(mp, a) * Map.fetch!(mp, b) * Map.fetch!(mp, c) end)
-    |> Enum.sum()
+    around_point(t, ps, acc + n * (n - 1))
   end
 
   defp group_points([], acc), do: acc
-  defp group_points([{0, 0} | t], acc), do: group_points(t, acc)
 
-  defp group_points([{0, y} | t], acc) do
-    k =
-      cond do
-        y > 0 -> {0, 1}
-        :else -> {0, -1}
-      end
+  defp group_points([{0, _} | t], acc),
+    do: group_points(t, Map.update(acc, {0, 1}, 1, fn z -> z + 1 end))
 
-    group_points(t, Map.update(acc, k, 1, fn z -> z + 1 end))
-  end
-
-  defp group_points([{x, 0} | t], acc) do
-    k =
-      cond do
-        x > 0 -> {1, 0}
-        :else -> {-1, 0}
-      end
-
-    group_points(t, Map.update(acc, k, 1, fn z -> z + 1 end))
-  end
+  defp group_points([{_, 0} | t], acc),
+    do: group_points(t, Map.update(acc, {1, 0}, 1, fn z -> z + 1 end))
 
   defp group_points([{x, y} | t], acc) do
     g = Integer.gcd(x, y)
-    k = {div(x, g), div(y, g)}
+    gx = div(x, g)
+    gy = div(y, g)
+
+    k =
+      cond do
+        gx > 0 and gy > 0 -> {gx, gy}
+        gx < 0 and gy > 0 -> {gx, gy}
+        gx > 0 and gy < 0 -> {-gx, -gy}
+        gx < 0 and gy < 0 -> {-gx, -gy}
+      end
+
     group_points(t, Map.update(acc, k, 1, fn z -> z + 1 end))
   end
 end
