f6376b74d4ae1a88408c1f4645a271333c5a4575
tt67wq <807643203@qq.com>
2019-03-21 15:02:20 +0800

    Still can't get this right...

--- euler_elixir/euler184.ex
+++ euler_elixir/euler184.ex
@@ -5,16 +5,6 @@ defmodule Euler184 do
   require Integer
   require Logger
 
-  # 判断原点是否在点a,b,c组成的三角形内部
-  def inner_origin({ax, ay}, {bx, by}, {cx, cy}) do
-    sign = (bx - ax) * (cy - ay) - (by - ay) * (cx - ax)
-    signAB = (bx - ax) * (0 - ay) - (by - ay) * (0 - ax)
-    signCA = (ax - cx) * (0 - cy) - (ay - cy) * (0 - cx)
-    signBC = (cx - bx) * (0 - cy) - (cy - by) * (0 - cx)
-
-    false not in [signAB * sign > 0, signCA * sign > 0, signBC * sign > 0]
-  end
-
   def get_points(n) do
     l = n - 1
 
@@ -29,34 +19,43 @@ defmodule Euler184 do
   # 向量差积
   def vector_cross({x1, y1}, {x2, y2}), do: x1 * y2 - x2 * y1
 
-  def test(n) do
+  def now(), do: :os.system_time(:milli_seconds)
+
+  def run(n) do
+    start = now()
     points = get_points(n)
 
+    Logger.debug("point fetched!")
     pnum = Enum.count(points)
-    p1 = around_point(points, points, 0)
 
     gp = group_points(points, %{})
-    Logger.info("#{inspect(gp)}")
-    # group_points(points, %{})
+    Logger.debug("group point finish!")
+
+    p1 = around_point(gp, pnum)
+
     p2 =
       gp
       |> Map.values()
-      |> Enum.map(fn x -> div(x, 2) * div(x, 2) * (pnum - x) + div(x * (x - 1) * (x - 2), 6) end)
+      |> Enum.map(fn x ->
+        div(x, 2) * div(x, 2) * (pnum - x) + div(div(x, 2) * (div(x, 2) - 1), 2) * (pnum - x) +
+          div(x * (x - 1) * (x - 2), 6)
+      end)
       |> Enum.sum()
 
     Logger.info("#{p1}, #{p2}")
 
-    div(pnum * (pnum - 1) * (pnum - 2), 6) - div(p1, 2) - p2
+    res = div(pnum * (pnum - 1) * (pnum - 2), 6) - div(p1, 2) - p2
+    IO.puts(res)
+    IO.puts("timeuse => #{now() - start} milliseconds")
   end
 
-  defp around_point([], _, acc), do: acc
-
-  defp around_point([h | t], ps, acc) do
-    n =
-      Enum.filter(ps, fn x -> vector_cross(x, h) < 0 end)
-      |> Enum.count()
-
-    around_point(t, ps, acc + n * (n - 1))
+  defp around_point(gp, pnum) do
+    Map.to_list(gp)
+    |> Enum.map(fn {_, c} ->
+      n = div(pnum - c, 2)
+      c * n * (n - 1)
+    end)
+    |> Enum.sum()
   end
 
   defp group_points([], acc), do: acc
