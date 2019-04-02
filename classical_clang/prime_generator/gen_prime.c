274663eec5cb55deb3c96f71a7f4f03b3e4f96ab
tt67wq <807643203@qq.com>
2019-04-02 09:38:29 +0800

    LOTS of changes. period

--- classical_clang/prime_generator/gen_prime.c
+++ classical_clang/prime_generator/gen_prime.c
@@ -20,15 +20,43 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef kvec_t(int) seq;
+typedef kvec_t(int) array;
+
+int isNotPrime(uint64_t x, array *vec) {
+        if ((x & 1) == 0)
+                return !(x == 2);
+        return vec->a[x >> 1];
+}
+
+void fillPrimes(uint64_t size, array *vec) {
+        uint64_t half = (size >> 1) + 1;
+        kv_resize(int, *vec, half);
+
+        // 1 is not prime
+        vec->a[0] = 1;
+
+        // 筛法
+        for (uint64_t i = 1; 2 * i * i < half; i++) {
+                if (!(vec->a[i])) {
+                        // mark all its multiples as false
+                        uint64_t current = 3 * i + 1;
+                        while (current < half) {
+                                vec->a[current] = 1;
+                                current += 2 * i + 1;
+                        }
+                }
+        }
+}
 
 int main() {
-        seq vec;
+        array vec;
         kv_init(vec);
-        kv_resize(int, vec, 10);
-        kv_push(int, vec, 1);
-        kv_push(int, vec, 2);
-        printf("%d\n", kv_a(int, vec, 1));
-        printf("%zu\n", kv_size(vec));
+
+        fillPrimes(300, &vec);
+        for (int i = 0; i < 300; i++) {
+                if (!isNotPrime(i, &vec))
+                        printf("%d ", i);
+        }
+
         return 0;
 }
