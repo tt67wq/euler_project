a6dbc26516cfc2f80e8657991dff3ed9e6710cdb
tt67wq <807643203@qq.com>
2019-04-11 16:36:06 +0800

    third time's a charm

--- euler_clang/euler192/euler192.c
+++ euler_clang/euler192/euler192.c
@@ -21,13 +21,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef kvec_t(uint64_t) array;
+#define TOP 1e12
+
+typedef kvec_t(int64_t) array;
 
 int nearest_sqrt(int n) {
         int i = 1;
         for (; i * i < n; i++)
                 ;
-        return i - 1;
+        return abs((i - 1) * (i - 1) - n) > abs(i * i - n) ? i : i - 1;
 }
 
 uint64_t gcd(uint64_t m, uint64_t n) {
@@ -41,38 +43,114 @@ uint64_t gcd(uint64_t m, uint64_t n) {
 void best_approximations(array *m, array *n, int k) {
         for (int i = 1; i < 200; i++) {
 
-                kv_push(uint64_t, *n, i);
+                kv_push(int64_t, *n, i);
 
                 int m1 = (int)floor(sqrt(k) * i);
                 int m2 = (int)ceil(sqrt(k) * i);
-                int t = abs(m1 * m1 - i * i * k) > abs(m2 * m2 - i * i * k) ? m2 : m1;
-                kv_push(uint64_t, *m, t);
+                int t = llabs(m1 * m1 - i * i * k) > llabs(m2 * m2 - i * i * k) ? m2 : m1;
+                kv_push(int64_t, *m, t);
         }
 }
 
-int main() {
-        array m, n;
-        kv_init(m);
-        kv_init(n);
-        int k = 13;
+int64_t extend_gcd(int64_t a, int64_t b) {
+        int64_t a1 = 0;
+        int64_t a2 = 1;
 
-        best_approximations(&m, &n, k);
-        double d = 100.0;
-        uint64_t a, b;
+        int64_t b1 = 1;
+        int64_t b2 = 0;
 
-        for (int i = 0; i < kv_size(m); i++) {
-                uint64_t mi = kv_A(m, i);
-                uint64_t ni = kv_A(n, i);
-                uint64_t g = gcd(mi, ni);
+        while (b) {
+                int64_t c = a / b;
+                int64_t tmp = a;
+                a = b;
+                b = tmp % b;
 
-                double t = fabs((mi * mi) / (double)(ni * ni) - k);
-                if (t < d) {
-                        a = mi / g;
-                        b = ni / g;
-                        d = t;
-                }
+                tmp = a1;
+                a1 = a2 - c * a1;
+                a2 = tmp;
 
-                printf("%d ==> %llu/%llu, nearest => %llu/%llu\n", i + 1, mi / g, ni / g, a, b);
+                tmp = b1;
+                b1 = b2 - c * b1;
+                b2 = tmp;
         }
+        return a2;
+}
+
+int64_t niyuan(int64_t a, int64_t b) {
+        int64_t g = extend_gcd(a, b);
+        return g > 0 ? g : b + g;
+}
+
+void approximations(int k, array *ms, array *ns) {
+        int64_t m = kv_A(*ms, kv_size(*ms) - 1);
+        int64_t n = kv_A(*ns, kv_size(*ns) - 1);
+        printf("%llu/%llu\n", m, n);
+        if (n > TOP) {
+                kv_pop(*ms);
+                kv_pop(*ns);
+                return;
+        }
+
+        double d1 = (double)m / n;
+        if (d1 * d1 > k) {
+                // bigger
+                int64_t y0 = niyuan(m, n);
+                int64_t x0 = (m * y0 - 1) / n;
+                for (int64_t i = y0 + n, j = x0 + m;; i += n, j += m) {
+                        double d2 = (double)j / i;
+                        if (fabs(d1 * d1 - k) > fabs(d2 * d2 - k)) {
+                                kv_push(int64_t, *ms, j);
+                                kv_push(int64_t, *ns, i);
+                                break;
+                        }
+                }
+        } else {
+                // smaller
+                int64_t x0 = niyuan(n, m);
+                int64_t y0 = (n * x0 - 1) / m;
+                for (int64_t i = x0 + m, j = y0 + n;; i += m, j += n) {
+                        double d2 = (double)i / j;
+                        if (fabs(d1 * d1 - k) > fabs(d2 * d2 - k)) {
+                                kv_push(int64_t, *ms, i);
+                                kv_push(int64_t, *ns, j);
+                                break;
+                        }
+                }
+        }
+        approximations(k, ms, ns);
+}
+
+int main() {
+        array ms, ns;
+        int j = 1;
+        uint64_t s = 0;
+        for (int k = 13; k <= 13; k++) {
+                while (j * j < k)
+                        j++;
+                if (j * j == k)
+                        continue;
+
+                kv_init(ms);
+                kv_init(ns);
+                kv_push(int64_t, ns, 1);
+                kv_push(int64_t, ms, (int64_t)nearest_sqrt(k));
+                approximations(k, &ms, &ns);
+                int64_t fm = (uint64_t)kv_pop(ms);
+                int64_t fn = (uint64_t)kv_pop(ns);
+
+                printf("%d => %llu/%llu\n", k, fm, fn);
+                s += fn;
+        }
+        printf("%llu\n", s);
+
+        /* kv_init(ms); */
+        /* kv_init(ns); */
+        /* int k = 13; */
+        /* kv_push(int64_t, ns, 1); */
+        /* kv_push(int64_t, ms, 4); */
+        /* approximations(k, &ms, &ns); */
+        /* for (int i = 0; i < kv_size(ms); i++) { */
+        /*         printf("%llu / %llu\n", kv_A(ms, i), kv_A(ns, i)); */
+        /* } */
         return 0;
 }
