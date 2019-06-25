5bafc796bcfbec15e91e7d9cb16304d9c84289ca
tt67wq <alex@mys4s.cn>
2019-06-25 11:31:45 +0800

    9rwqslu360

--- euler_clang/euler213/euler213.c
+++ euler_clang/euler213/euler213.c
@@ -6,7 +6,7 @@
  *    Description:  欧拉计划213题
  *
  *        Version:  1.0
- *        Created:  2019-06-24
+ *        Created:  2019-06-25
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -17,11 +17,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-#define N 100000
-
-int fleas[900];
-int grid[900];
 
 int is_corner(int pos) {
         int res = 0;
@@ -56,155 +53,87 @@ int is_board(int pos) {
         return 0;
 }
 
-void jump_once() {
+void jumps(double *state) {
 
-        for (int i = 0; i < 900; i++) {
-                int d = 0;
-                if (is_corner(fleas[i])) {
-                        // 角落
-                        d = (int)(2.0 * rand() / (RAND_MAX + 1.0));
-                        if (fleas[i] == 0) {
-                                switch (d) {
-                                case 0:
-                                        fleas[i] = 1;
-                                        break;
-                                default:
-                                        fleas[i] = 30;
-                                        break;
+        int index = 50;
+        while (index) {
+                double buffer[900] = {0.0};
+
+                for (int i = 0; i < 900; i++) {
+                        if (state[i] > 0.0) {
+                                double base = state[i];
+                                if (is_corner(i)) {
+                                        double tmp = base * 0.5;
+                                        if (i == 0) {
+                                                buffer[1] += tmp;
+                                                buffer[30] += tmp;
+                                        } else if (i == 29) {
+                                                buffer[28] += tmp;
+                                                buffer[59] += tmp;
+                                        } else if (i == 870) {
+                                                buffer[871] += tmp;
+                                                buffer[840] += tmp;
+                                        } else {
+                                                buffer[869] += tmp;
+                                                buffer[898] += tmp;
+                                        }
+                                } else if (is_board(i)) {
+                                        double tmp = base * 1.0 / 3;
+                                        if (i < 30) {
+                                                buffer[i + 1] += tmp;
+                                                buffer[i - 1] += tmp;
+                                                buffer[i + 30] += tmp;
+                                        } else if (i % 30 == 0) {
+                                                buffer[i - 30] += tmp;
+                                                buffer[i + 30] += tmp;
+                                                buffer[i + 1] += tmp;
+                                        } else if (i % 30 == 29) {
+                                                buffer[i - 30] += tmp;
+                                                buffer[i + 30] += tmp;
+                                                buffer[i - 1] += tmp;
+                                        } else {
+                                                buffer[i + 1] += tmp;
+                                                buffer[i - 1] += tmp;
+                                                buffer[i - 30] += tmp;
+                                        }
+                                } else {
+                                        double tmp = base * 0.25;
+                                        buffer[i + 1] += tmp;
+                                        buffer[i - 1] += tmp;
+                                        buffer[i + 30] += tmp;
+                                        buffer[i - 30] += tmp;
                                 }
-                        } else if (fleas[i] == 29) {
-                                switch (d) {
-                                case 0:
-                                        fleas[i] = 28;
-                                        break;
-                                default:
-                                        fleas[i] = 59;
-                                        break;
-                                }
-                        } else if (fleas[i] == 870) {
-                                switch (d) {
-                                case 0:
-                                        fleas[i] = 840;
-                                        break;
-                                default:
-                                        fleas[i] = 871;
-                                        break;
-                                }
-                        } else {
-                                switch (d) {
-                                case 0:
-                                        fleas[i] = 869;
-                                        break;
-                                default:
-                                        fleas[i] = 898;
-                                        break;
-                                }
-                        }
-                } else if (is_board(fleas[i])) {
-                        d = (int)(3.0 * rand() / (RAND_MAX + 1.0));
-                        if (fleas[i] < 30) {
-                                switch (d) {
-                                case 0:
-                                        fleas[i] += 1;
-                                        break;
-                                case 1:
-                                        fleas[i] += 30;
-                                        break;
-                                default:
-                                        fleas[i] -= 1;
-                                        break;
-                                }
-                        } else if (fleas[i] % 30 == 0) {
-                                switch (d) {
-                                case 0:
-                                        fleas[i] -= 30;
-                                        break;
-                                case 1:
-                                        fleas[i] += 1;
-                                        break;
-                                default:
-                                        fleas[i] += 30;
-                                        break;
-                                }
-                        } else if (fleas[i] % 30 == 29) {
-                                switch (d) {
-                                case 0:
-                                        fleas[i] -= 30;
-                                        break;
-                                case 1:
-                                        fleas[i] -= 1;
-                                        break;
-                                default:
-                                        fleas[i] += 30;
-                                        break;
-                                }
-                        } else {
-                                switch (d) {
-                                case 0:
-                                        fleas[i] -= 1;
-                                        break;
-                                case 1:
-                                        fleas[i] -= 30;
-                                        break;
-                                default:
-                                        fleas[i] += 1;
-                                        break;
-                                }
-                        }
-                } else {
-                        d = (int)(4.0 * rand() / (RAND_MAX + 1.0));
-                        switch (d) {
-                        case 0:
-                                fleas[i] -= 30;
-                                break;
-                        case 1:
-                                fleas[i] += 1;
-                                break;
-                        case 2:
-                                fleas[i] += 30;
-                                break;
-                        case 3:
-                                fleas[i] -= 1;
-                                break;
                         }
                 }
+                index--;
+                memcpy(state, buffer, sizeof(double) * 900);
         }
         return;
 }
 
-int expectation() {
-        int counter = 0;
-        // init
-        int grid[900] = {0};
-        for (int i = 0; i < 900; i++)
-                fleas[i] = i;
-
-        // jump 50 times;
-        for (int i = 0; i < 3; i++)
-                jump_once();
-
-        for (int j = 0; j < 900; j++) {
-                /* printf("fleas[%d] = %d\n", j, fleas[j]); */
-                grid[fleas[j]] += 1;
-        }
-
-        for (int j = 0; j < 900; j++) {
-                if (grid[j] == 0)
-                        counter++;
-        }
-
-        return counter;
-}
-
 int main(int argc, const char *argv[]) {
-        srand((int)time(0));
+        double res[900] = {0};
+        double expectation = 0.0;
 
-        int total = 0;
-        for (int i = 0; i < N; i++) {
-                total += expectation();
+        // init
+        for (int j = 0; j < 900; j++) {
+                res[j] = 1.0;
         }
-        printf("%0.6f\n", (float)total / N);
+
+        for (int i = 0; i < 900; i++) {
+                double state[900] = {0.0};
+                state[i] = 1.0;
+                jumps(state);
+
+                for (int j = 0; j < 900; j++) {
+                        res[j] *= (1.0 - state[j]);
+                }
+        }
+        for (int i = 0; i < 900; i++) {
+                expectation += res[i];
+        }
+        printf("%0.7f\n", expectation);
         printf("330.721154\n");
 
-        return 1;
+        return 0;
 }
