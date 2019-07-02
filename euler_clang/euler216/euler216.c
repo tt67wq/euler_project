89989a400ccf9c2c885538fab084b1591bf8d569
tt67wq <alex@mys4s.cn>
2019-07-02 14:50:20 +0800

    xut710ausd

---
+++ euler_clang/euler216/euler216.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  euler216.c
+ *
+ *    Description:  欧拉计划216题
+ *
+ *        Version:  1.0
+ *        Created:  2019-07-02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  fangyuan
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(int argc, const char *argv[]) {
+	// 先研究Tonelli–Shanks算法再来研究这一题
+	return 0;
+}
