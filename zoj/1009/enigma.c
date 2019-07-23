7f83b62d7b605bb43766c22c113fb418f7be6c8f
tt67wq <alex@mys4s.cn>
2019-07-23 10:51:53 +0800

    sygmpia4yq

--- zoj/1009/enigma.c
+++ zoj/1009/enigma.c
@@ -14,7 +14,7 @@
  *
  * =====================================================================================
  */
-
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,48 +28,79 @@ int shift0[26];
 int shift1[26];
 int shift2[26];
 
-char crypto[10][BUFSIZ];
+char crypto[1000][200];
 
 void gen_shift(char *rotor, int *shift) {
         int i;
         for (i = 0; i < rotor_len; i++)
-                shift[i] = base[i] - rotor[i];
+                if (rotor[i] - base[i] >= 0)
+                        shift[i] = rotor[i] - base[i];
+                else
+                        shift[i] = rotor_len + rotor[i] - base[i];
 }
 
 void decode(char *crypto) {
-        int i, j;
-        int len;
+        int i, j, len, tmp;
         len = strlen(crypto);
-        for (i = 0; i < len; i++) {
-                for (j = 0; j < 26; j++) {
-                        if (base[j] + shift2[i] == crypto[i]) {
-                                crypto[i] = base[j];
-                                break;
-                        }
-                }
-                for (j = 0; j < 26; j++) {
-                        if (base[j] + shift1[i] == crypto[i]) {
-                                crypto[i] = base[j];
-                                break;
-                        }
-                }
+        i = 0;
 
-                for (j = 0; j < 26; j++) {
-                        if (base[j] + shift0[i] == crypto[i]) {
-                                crypto[i] = base[j];
-                                break;
-                        }
-                }
+	char _rotor0[26];
+	char _rotor1[26];
+	char _rotor2[26];
 
-                /* shift */
+	strcpy(_rotor0, rotor0);
+	strcpy(_rotor1, rotor1);
+	strcpy(_rotor2, rotor2);
+	
+        while (i < len) {
                 for (j = 0; j < rotor_len; j++) {
-                        shift0[(j + 1) % rotor_len] = shift0[j];
-                        if (i % rotor_len == 0)
-                                shift1[(j + 1) % rotor_len] = shift1[j];
-                        if (i % (rotor_len * rotor_len) == 0)
-                                shift2[(j + 1) % rotor_len] = shift2[j];
+                        if (crypto[i] == _rotor2[j]) {
+                                crypto[i] = base[j];
+                                break;
+                        }
+                }
+                for (j = 0; j < rotor_len; j++) {
+                        if (crypto[i] == _rotor1[j]) {
+                                crypto[i] = base[j];
+                                break;
+                        }
+                }
+                for (j = 0; j < rotor_len; j++) {
+                        if (crypto[i] == _rotor0[j]) {
+                                crypto[i] = base[j];
+                                break;
+                        }
+                }
+
+                i++;
+                for (j = 0; j < rotor_len; j++) {
+                        tmp = j - i;
+                        while (tmp < 0)
+                                tmp += rotor_len;
+
+                        _rotor0[j] = base[(j + shift0[tmp]) % rotor_len];
+                }
+                if (i % rotor_len == 0) {
+                        for (j = 0; j < rotor_len; j++) {
+                                tmp = j - i / rotor_len;
+                                while (tmp < 0)
+                                        tmp += rotor_len;
+
+                                _rotor1[j] = base[(j + shift1[tmp]) % rotor_len];
+                        }
+                }
+                if (i % (rotor_len * rotor_len) == 0) {
+                        for (j = 0; j < rotor_len; j++) {
+                                tmp = j - i / (rotor_len * rotor_len);
+                                while (tmp < 0)
+                                        tmp += rotor_len;
+
+                                _rotor2[j] = base[(j + shift2[tmp]) % rotor_len];
+                        }
                 }
         }
+        for (i = 0; i < len; i++)
+                crypto[i] = tolower(crypto[i]);
 }
 
 int main() {
@@ -78,6 +109,10 @@ int main() {
         while (scanf("%d", &rotor_len) != EOF) {
                 if (!rotor_len)
                         break;
+                if (index != 1)
+                        printf("\n");
+                printf("Enigma %d:\n", index++);
+
                 scanf("%s", rotor0);
                 scanf("%s", rotor1);
                 scanf("%s", rotor2);
@@ -90,12 +125,8 @@ int main() {
                 for (i = 0; i < n; i++) {
                         scanf("%s", crypto[i]);
                         decode(crypto[i]);
-                }
-                printf("Enigma %d:\n", index);
-                for (i = 0; i < n; i++) {
                         printf("%s\n", crypto[i]);
                 }
-                printf("\n");
         }
 
         return 0;
