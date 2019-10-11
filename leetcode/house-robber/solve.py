7aa988bb5ba680e3440d122bb31b3cea68c96b52
tt67wq <alex@mys4s.cn>
2019-10-11 11:13:42 +0800

    fzjwh8qj6j

---
+++ leetcode/house-robber/solve.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# author: wq
+# description: ""
+import hashlib
+CACHE = {}
+
+
+class Solution(object):
+    def fp(self, nums):
+        s = ",".join([str(x) for x in nums]).encode()
+        return hashlib.md5(s).hexdigest()
+
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if sum(nums) == 0:
+            return 0
+        if len(nums) == 0:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        if len(nums) == 2:
+            return max(nums)
+
+        p = self.fp(nums)
+        if CACHE.get(p):
+            return CACHE[p]
+
+        res = max(nums[0]+self.rob(nums[2:]), nums[1]+self.rob(nums[3:]))
+        CACHE[p] = res
+        return res
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
