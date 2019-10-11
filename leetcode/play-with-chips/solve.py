e019ae18d84208ed96ba09acc5e3cf808893d860
tt67wq <alex@mys4s.cn>
2019-10-11 14:48:04 +0800

    qb54we68hp

---
+++ leetcode/play-with-chips/solve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# author: wq
+# description: ""
+
+
+class Solution:
+    def minCostToMoveChips(self, chips):
+        mp = {0: 0, 1: 0}
+        for c in chips:
+            mp[c % 2] += 1
+        return min(list(mp.values()))
+
+
+def main():
+    s = Solution()
+    while 1:
+        print(s.minCostToMoveChips([int(x) for x in input().split(",")]))
+
+
+if __name__ == '__main__':
+    main()
