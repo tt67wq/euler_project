ca40d6e5f78c8a1170822a8a35f3be460cc01bda
tt67wq <alex@mys4s.cn>
2019-10-12 15:28:37 +0800

    bjj45tu3v3

---
+++ leetcode/integer-replacement/solve.c
@@ -0,0 +1,48 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  solve.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2019-10-12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  fangyuan
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int ui;
+
+ui min(ui a, ui b) { return a > b ? b : a; }
+
+ui integerReplacement(ui n) {
+        ui s = 0;
+        while (n != 1) {
+                if (n % 2 == 0) {
+                        n >>= 1;
+                        s++;
+                } else {
+                        s++;
+                        s += min(integerReplacement(n - 1),
+                                 integerReplacement(n + 1));
+                        break;
+                }
+        }
+        return s;
+}
+
+int main() {
+        int n;
+        while (1) {
+                scanf("%d", &n);
+                printf("%d\n", integerReplacement(n));
+        }
+        return 0;
+}
