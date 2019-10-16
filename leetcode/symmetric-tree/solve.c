a0536fd67f40c1acfe3e1d667649de2c9e42f113
tt67wq <alex@mys4s.cn>
2019-10-16 10:21:35 +0800

    hbo6mb4wj3

---
+++ leetcode/symmetric-tree/solve.c
@@ -0,0 +1,79 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  solve.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2019-10-16
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  fangyuan
+ *
+ * =====================================================================================
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Definition for a binary tree node. */
+struct TreeNode {
+        int val;
+        struct TreeNode *left;
+        struct TreeNode *right;
+};
+
+void exchange(struct TreeNode *node) {
+        if (node != NULL) {
+                struct TreeNode *tmp;
+
+                tmp = node->left;
+                node->left = node->right;
+                node->right = tmp;
+
+                exchange(node->left);
+                exchange(node->right);
+        }
+}
+
+bool cmpTree(struct TreeNode *node1, struct TreeNode *node2) {
+        if (node1 == NULL && node2 == NULL)
+                return true;
+
+        if (node1 == NULL && node2 != NULL)
+                return false;
+
+        if (node1 != NULL && node2 == NULL)
+                return false;
+
+        if (node1->val != node2->val)
+                return false;
+
+        return cmpTree(node1->left, node2->left) &&
+               cmpTree(node1->right, node2->right);
+}
+
+struct TreeNode *copyTree(struct TreeNode *node) {
+        struct TreeNode *new = NULL;
+        if (node == NULL)
+                return NULL;
+        new = (struct TreeNode *)calloc(1, sizeof(struct TreeNode));
+        new->val = node->val;
+        new->left = copyTree(node->left);
+        new->right = copyTree(node->right);
+        return new;
+}
+
+bool isSymmetric(struct TreeNode *root) {
+        struct TreeNode *origin;
+
+        origin = copyTree(root);
+        exchange(root);
+
+        return cmpTree(origin, root);
+}
+
+int main() { return 0; }
