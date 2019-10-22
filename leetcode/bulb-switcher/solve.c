31ddf944ead2f15910b6077310d562e281730c12
tt67wq <alex@mys4s.cn>
2019-10-22 15:01:09 +0800

    9pghygdryw

---
+++ leetcode/bulb-switcher/solve.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  solve.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2019-10-22
+ *       Revision:  none
+ *       Compiler:  clang
+ *
+ *         Author:
+ *
+ * =====================================================================================
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int bulbSwitch(int n) { return (int)sqrt(n); }
+
+int main() { return 0; }
