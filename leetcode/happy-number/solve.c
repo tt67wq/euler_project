cd381493e11623d0022515872a441cf4aeaebd5e
tt67wq <alex@mys4s.cn>
2019-10-25 13:48:38 +0800

    r2bakmzqsm

---
+++ leetcode/happy-number/solve.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  solve.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2019-10-25
+ *       Revision:  none
+ *       Compiler:  clang
+ *
+ *         Author:
+ *
+ * =====================================================================================
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool isHappy(int n) {}
+
+int main() { return 0; }
