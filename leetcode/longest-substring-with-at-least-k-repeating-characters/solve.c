0e00fe35bc897987b155af8ccc4b181dacd55585
tt67wq <alex@mys4s.cn>
2019-11-05 15:48:50 +0800

    ys1h8rhxcq

--- leetcode/longest-substring-with-at-least-k-repeating-characters/solve.c
+++ leetcode/longest-substring-with-at-least-k-repeating-characters/solve.c
@@ -25,15 +25,19 @@ int process(char *s, int k, int l, int h) {
 
         int i, t1, t2;
         int vis[26] = {0};
+
+        /* 字母计数 */
         for (i = l; i <= h; i++)
                 vis[s[i] - 'a']++;
 
+        /* 裁剪两端的计数不够的字母 */
         while (h - l + 1 >= k && vis[s[l] - 'a'] < k)
                 l++;
 
         while (h - l + 1 >= k && vis[s[h] - 'a'] < k)
                 h--;
 
+        /* 如果有计数不够的字母夹在中间，分割开之后处理 */
         for (i = l; i <= h; i++) {
                 if (vis[s[i] - 'a'] < k) {
                         t1 = process(s, k, l, i - 1);
