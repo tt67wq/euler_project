9d33420cf69d94ae1c692c69957c12e4b6700c78
tt67wq <alex@mys4s.cn>
2020-09-16 09:17:50 +0800

    不当家不知柴米贵，不拍照不知自己肥。

--- euler/euler577/main.c
+++ euler/euler577/main.c
@@ -19,20 +19,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CN2(n) ((n) * ((n) + 1) / 2)
 // 3 -> 1    1
 // 4 -> 3    3
 // 5 -> 6    6
-// 6 -> 12   10 + 1 + 1
-void countHexagon(int n, long *count) {
-        for (int i = 0; i < n / 3 + 1; i++) {
-                long x = n - 3 * i + 1;
-                *count += (i * x * (x + 1) * (x + 2) / 6);
+// 6 -> 12   10 + 1*2
+
+void H(int n, long *count) {
+        int s = 1;
+        while (n > 0) {
+                *count += s * (long)CN2(n);
+                s++;
+                n -= 3;
         }
 }
 
+// void countHexagon(int n, long *count) {
+//         for (int i = 0; i < n / 3 + 1; i++) {
+//                 long x = n - 3 * i + 1;
+//                 *count += (i * x * (x + 1) * (x + 2) / 6);
+//         }
+// }
+
 int main() {
-        long count = 0;
-        countHexagon(12345, &count);
+        long count = 0, total = 0;
+
+        for (int i = 3; i <= 12345; i++) {
+                H(i, &count);
+                total += count;
+        }
         printf("%ld\n", count);
         return 0;
 }
