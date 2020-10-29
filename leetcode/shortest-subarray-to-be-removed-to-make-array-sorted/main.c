e89c12583df9153674c30cd167075df8f8bd1503
tt67wq <alex@mys4s.cn>
2020-10-29 10:39:30 +0800

    你以为今天是最糟的一天么，明天会让你改变这个看法的。

--- leetcode/shortest-subarray-to-be-removed-to-make-array-sorted/main.c
+++ leetcode/shortest-subarray-to-be-removed-to-make-array-sorted/main.c
@@ -15,45 +15,40 @@
  * =====================================================================================
  */
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// check if [0, k] overlapping decreasing area
-bool check(int *arr, int size, int k) {
-        if (k == size - 1)
-                return true;
-        int lo = 0, hi = size - 1;
-        while (lo < hi && arr[lo] <= arr[lo + 1])
-                ++lo;
-        while (hi > lo && arr[hi] >= arr[hi - 1])
-                --hi;
-
-        // [0,lo][lo+1,hi-1][hi,size-1]
-        if (hi <= k || size - lo - 1 <= k)
-                return true;
-
-        if (hi - lo - 1 > k)
-                return false;
-
-        for (int i = fmax(0, hi - k - 1); i <= lo && i + k + 1 < size; ++i) {
-                if (arr[i] <= arr[i + k + 1])
-                        return true;
-        }
-        return false;
-}
+#define MIN(a, b) (a) > (b) ? (b) : (a)
 
 int findLengthOfShortestSubarray(int *arr, int arrSize) {
-        int lo = 0, hi = arrSize - 1;
-        while (lo < hi) {
-                int mid = (lo + hi) >> 1;
-                if (check(arr, arrSize, mid)) {
-                        hi = mid;
+        int left = 0;
+
+        while (left + 1 < arrSize && arr[left] <= arr[left + 1]) {
+                ++left;
+        }
+
+        if (left == arrSize - 1) {
+                return 0;
+        }
+
+        int right = arrSize - 1;
+        while (right > 0 && arr[right - 1] <= arr[right]) {
+                --right;
+        }
+
+        int res = MIN(arrSize - left - 1, right);
+
+        int i = 0, j = right;
+
+        while (i <= left && j <= arrSize - 1) {
+                if (arr[i] <= arr[j]) {
+                        res = MIN(res, j - i - 1);
+                        ++i;
                 } else {
-                        lo = mid + 1;
+                        ++j;
                 }
         }
-        return lo;
+        return res;
 }
 
 int main() { return 0; }
