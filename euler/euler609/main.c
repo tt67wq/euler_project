c7517371d5e1065d21b194c8fd017fd51bd1108c
tt67wq <alex@mys4s.cn>
2020-11-06 10:20:18 +0800

    人为什么叫人类，因为人活着就是累。

--- euler/euler609/main.c
+++ euler/euler609/main.c
@@ -21,12 +21,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 100L
 #define IS_PRIME(a) (is_in_sieve((a)))
 
+const uint32_t MAX = 1e5;
+const uint32_t MOD = 1e9 + 7;
 static char SIEVE[MAX / 16 + 1];
-static uint32_t PRIMES[10000];
-static uint32_t PRIME_SIZE = 0;
+static uint32_t PRIMES[10000000];
+static uint32_t PRIMES_SIZE = 0;
 
 static inline bool is_in_sieve(long idx) { return !(SIEVE[idx / 16] & (1 << (idx % 16 / 2))); }
 static inline void remove_from_sieve(long idx) { SIEVE[idx / 16] |= (1 << (idx % 16 / 2)); }
@@ -41,22 +42,42 @@ void primeSieve() {
                         }
                 }
         }
+        PRIMES[PRIMES_SIZE++] = 2;
+
+        for (int i = 3; i < MAX; i += 2) {
+                if (IS_PRIME(i)) {
+                        PRIMES[PRIMES_SIZE++] = i;
+                }
+        }
+        printf("%d\n", PRIMES_SIZE);
 }
 
 int main() {
         primeSieve();
-        PRIMES[PRIME_SIZE++] = 2;
-
-        for (int i = 3; i < MAX; i += 2) {
-                if (IS_PRIME(i)) {
-                        PRIMES[PRIME_SIZE++] = i;
-                } else {
-                        // printf("(%d) ", i);
+        printf("sieve finished\n");
+        int pi[MAX + 1] = {0};
+        int p[100] = {0};
+        for (int i = 0; i < PRIMES_SIZE; i++) {
+                pi[PRIMES[i]] = 1;
+        }
+        for (int i = 1; i < MAX; i++) {
+                pi[i] += pi[i - 1];
+                for (int w = i, cnt = 0; w > 0; w = pi[w]) {
+                        if (pi[w] == pi[w - 1]) { // 非素数
+                                ++cnt;
+                        }
+                        if (w != i) {
+                                p[cnt] += 1;
+                        }
                 }
         }
-        for (int i = 0; i < PRIME_SIZE; i++) {
-                printf("%d ", PRIMES[i]);
+        uint64_t ans = 1;
+        for (int i = 0; i <= 100; ++i) {
+                if (p[i] == 0) {
+                        break;
+                }
+                ans = ans * (p[i] % MOD) % MOD;
         }
-        printf("\n");
+        printf("%lu\n", ans);
         return 0;
 }
