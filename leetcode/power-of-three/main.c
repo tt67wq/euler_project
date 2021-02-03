026f9d89f027e01dee61474758bb67c3f89f66eb
tt67wq <alex_wan@jiliguala.com>
2021-02-03 09:38:21 +0800

    一想到你，我的丑脸，就泛起微笑。

---
+++ leetcode/power-of-three/main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2021-02-03
+ *       Revision:  none
+ *       Compiler:  clang
+ *
+ *         Author:
+ *
+ * =====================================================================================
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool isPowerOfThree(int n) { return n > 0 && 1162261467 % n == 0; }
+
+int main() { return 0; }
